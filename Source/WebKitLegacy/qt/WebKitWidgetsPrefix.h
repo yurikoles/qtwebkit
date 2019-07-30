7b2c892d5b61aede6567a15aa84621e3697553db
Konstantin Tokarev <annulen@yandex.ru>
2019-07-30 03:23:07 +0300

    Force-include WebKitWidgetsPrefix.h on all platforms
    Change-Id: I55841d98757208a1c29d3948613bcc16995ff582

--- Source/WebKitLegacy/qt/WebKitWidgetsPrefix.h
+++ Source/WebKitLegacy/qt/WebKitWidgetsPrefix.h
@@ -1,12 +1,2 @@
-#if defined(HAVE_CONFIG_H) && HAVE_CONFIG_H && defined(BUILDING_WITH_CMAKE)
 #include "cmakeconfig.h"
-#endif
-
-
-#if OS(WINDOWS)
-
-#ifndef WEBCORE_EXPORT
-#define WEBCORE_EXPORT WTF_IMPORT_DECLARATION
-#endif // !WEBCORE_EXPORT
-
-#endif
+#include <WebCore/PlatformExportMacros.h>
