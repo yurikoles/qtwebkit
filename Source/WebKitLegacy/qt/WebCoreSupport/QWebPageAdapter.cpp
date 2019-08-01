e21bce93ef012a490047139dbe55f5b9cdc53605
Konstantin Tokarev <annulen@yandex.ru>
2019-08-01 15:29:18 +0300

    wip disable some code in QWebPageAdapter
    Change-Id: I8d7838fff806affea310182ecea7c4b71e596c62

--- Source/WebKitLegacy/qt/WebCoreSupport/QWebPageAdapter.cpp
+++ Source/WebKitLegacy/qt/WebCoreSupport/QWebPageAdapter.cpp
@@ -713,7 +713,7 @@ void QWebPageAdapter::inputMethodEvent(QInputMethodEvent *ev)
                 RenderObject* caretRenderer = frame.selection().caretRendererWithoutUpdatingLayout();
                 if (caretRenderer) {
                     QColor qcolor = a.value.value<QColor>();
-                    caretRenderer->style().setColor(Color(makeRGBA(qcolor.red(), qcolor.green(), qcolor.blue(), qcolor.alpha())));
+//                    caretRenderer->style().setColor(qcolor);
                 }
             }
             break;
@@ -1272,7 +1272,8 @@ QString QWebPageAdapter::contextMenuItemTagForAction(QWebPageAdapter::MenuAction
         *checkable = true;
         return contextMenuItemTagToggleMediaLoop();
     case ToggleMediaPlayPause:
-        return contextMenuItemTagMediaPlayPause();
+        return QString();
+//        return contextMenuItemTagMediaPlayPause();
     case ToggleMediaMute:
         *checkable = true;
         return contextMenuItemTagMediaMute();
