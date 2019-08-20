afaef1e41683f18a868b95b2a76900ad06bd73fb
Rastorguev <rastrelli1@yandex.ru>
2019-08-20 22:26:27 +0300

    FIX: Runloop timers work on threads
    Change-Id: I533e2a1b52911ee9fe6e976c851229aee9637ab7

--- Source/WTF/wtf/RunLoop.h
+++ Source/WTF/wtf/RunLoop.h
@@ -41,6 +41,11 @@
 #include <wtf/glib/GRefPtr.h>
 #endif
 
+#if PLATFORM(QT)
+#include <QMetaType>
+class QThread;
+#endif
+
 namespace WTF {
 
 class RunLoop : public FunctionDispatcher {
@@ -135,7 +140,8 @@ public:
 #elif PLATFORM(QT)
         static void timerFired(RunLoop*, int ID);
         int m_ID;
-        bool m_isRepeating;
+        bool m_isActive { false };
+        bool m_isRepeating { false };
 #elif USE(GLIB_EVENT_LOOP)
         void updateReadyTime();
         GRefPtr<GSource> m_source;
@@ -193,10 +199,15 @@ private:
     RetainPtr<CFRunLoopSourceRef> m_runLoopSource;
     RetainPtr<CFMachPortRef> m_port;
 #elif PLATFORM(QT)
+    class TimerObject;
+    class PerformWorkTimerObject;
+    PerformWorkTimerObject* m_performWorkTimer;
+    TimerObject* m_timer;
     typedef HashMap<int, TimerBase*> TimerMap;
     TimerMap m_activeTimers;
-    class TimerObject;
-    TimerObject* m_timerObject;
+    typedef HashMap<QThread*, TimerObject*> TimerObjectMap;
+    TimerObjectMap m_timerObjects;
+    Lock m_loopLock;
 #elif USE(GLIB_EVENT_LOOP)
     GRefPtr<GMainContext> m_mainContext;
     Vector<GRefPtr<GMainLoop>> m_mainLoops;
@@ -234,3 +245,7 @@ private:
 } // namespace WTF
 
 using WTF::RunLoop;
+
+#if PLATFORM(QT)
+Q_DECLARE_METATYPE(WTF::RunLoop::TimerBase*)
+#endif
