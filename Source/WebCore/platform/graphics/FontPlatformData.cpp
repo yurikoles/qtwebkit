5cd1b7395932c0532f131cafeeadcbe3e95b4bf6
Konstantin Tokarev <annulen@yandex.ru>
2019-09-05 20:35:37 +0300

    wip Get rid of Qt-specific FontPlatformData declaration
    See r200129 - we may need to handle more things

--- Source/WebCore/platform/graphics/FontPlatformData.cpp
+++ Source/WebCore/platform/graphics/FontPlatformData.cpp
@@ -76,12 +76,14 @@ FontPlatformData FontPlatformData::cloneWithSyntheticOblique(const FontPlatformD
     return copy;
 }
 
+#if !PLATFORM(QT)
 FontPlatformData FontPlatformData::cloneWithSize(const FontPlatformData& source, float size)
 {
     FontPlatformData copy(source);
     copy.m_size = size;
     return copy;
 }
+#endif
 #endif
 
 }
