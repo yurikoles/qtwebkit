7167a08993046553f9c6a244656466e9e2350599
Konstantin Tokarev <annulen@yandex.ru>
2019-10-03 22:42:00 +0300

    Optimize separated debug info with dwz
    Using dwz saves about 1Gb (~40%) for non-compressed debug file of
    libQt5WebKit.so, and ~50% of 7z-compressed debug package.
    
    Change-Id: I8489311de563ea70921825191deff810f95911b7

---
+++ Source/cmake/FindDwz.cmake
@@ -0,0 +1,38 @@
+# - Find dwz
+# This module looks for dwz. This module defines the
+# following values:
+
+#  DWZ_EXECUTABLE          - The full path to the dwz tool.
+#  DWZ_FOUND               - True if dwz has been found.
+#
+# Copyright (C) 2019 Konstantin Tokarev <annulen@yandex.ru>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
+# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+find_program(DWZ_EXECUTABLE NAMES dwz)
+
+# handle the QUIETLY and REQUIRED arguments and set DWZ_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Dwz DEFAULT_MSG DWZ_EXECUTABLE)
+
+mark_as_advanced(DWZ_EXECUTABLE)
