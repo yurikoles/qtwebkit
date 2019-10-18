ec942159ca0337bcb638d101a4fdf614cf60a8c4
Konstantin Tokarev <annulen@yandex.ru>
2019-10-18 18:34:10 +0300

    Fix compilation after r251117
    Change-Id: I8b296b0e91cd0f32cc09536baaa6ca600fe32105

--- Source/WebCore/platform/qt/PasteboardQt.cpp
+++ Source/WebCore/platform/qt/PasteboardQt.cpp
@@ -437,7 +437,7 @@ void Pasteboard::writeMarkup(const String&)
 {
 }
 
-void Pasteboard::writeCustomData(const PasteboardCustomData&)
+void Pasteboard::writeCustomData(const Vector<PasteboardCustomData>&)
 {
 }
 
