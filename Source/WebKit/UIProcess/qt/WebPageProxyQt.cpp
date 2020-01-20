4314cc42fcae5e654c6d9d858c09a56e491ade80
RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
2020-01-20 21:04:12 +0530

    User Agent Fix

--- Source/WebKit/UIProcess/qt/WebPageProxyQt.cpp
+++ Source/WebKit/UIProcess/qt/WebPageProxyQt.cpp
@@ -51,7 +51,7 @@ void WebPageProxy::platformInitialize()
 
 String WebPageProxy::standardUserAgent(const String& applicationNameForUserAgent)
 {
-    return String();
+    return UserAgentQt::standardUserAgent(applicationNameForUserAgent,2,11);
 }
 
 void WebPageProxy::saveRecentSearches(const String&, const Vector<WebCore::RecentSearch>&)
