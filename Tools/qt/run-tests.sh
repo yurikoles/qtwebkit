2380a05924f4cff4871ed36a20c078db6b2a4fa2
Konstantin Tokarev <annulen@yandex.ru>
2020-09-25 12:46:16 +0300

    Use proper fonts repo
    Change-Id: I677370906085ec04d271e4c411a25f628d31b479

---
+++ Tools/qt/run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export WEBKIT_OUTPUTDIR=$(pwd)/WebKitBuild-Docker
+export PATH=${WEBKIT_OUTPUTDIR}/DependenciesQT/Root/bin:$PATH
+set -e
+git clone https://github.com/qtwebkit/test-fonts.git
+export WEBKIT_TESTFONTS=$(pwd)/test-fonts
+ln -sf $(pwd) /var/tmp/qtwebkit-build
+Tools/Scripts/run-webkit-tests --qt -1 \
+        --no-build --clobber-old-results \
+        --no-show-results --no-new-test-results \
+        --child-processes=2 --results-directory=$(pwd)/layout-test-results
+tar zcf layout-tests.tar.gz $(pwd)/layout-test-results
