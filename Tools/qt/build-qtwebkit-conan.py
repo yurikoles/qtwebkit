c1ed9ce777cdf7a2ce4ac5d6104f0f1586b57389
Konstantin Tokarev <annulen@yandex.ru>
2020-10-13 03:22:20 +0300

    [Conan] qtproject repository should have highest priority
    Change-Id: Id09dab557560e8985e0fa23e329d6f709fa02c45

--- Tools/qt/build-qtwebkit-conan.py
+++ Tools/qt/build-qtwebkit-conan.py
@@ -191,7 +191,7 @@ conanfile_path = os.path.join(src_directory, "Tools", "qt", "conanfile.py")
 print("Path of build directory:" + build_directory)
 
 run_command("conan remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan")
-run_command("conan remote add -f qtproject https://api.bintray.com/conan/qtproject/conan")
+run_command("conan remote add -f qtproject https://api.bintray.com/conan/qtproject/conan --insert")
 
 if args.profile and args.compiler:
     sys.exit("Error: --compiler and --profile cannot be specified at the same time")
