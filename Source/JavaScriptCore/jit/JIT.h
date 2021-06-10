yurikoles/qtwebkit

commit 319860dee8cb729f6f41d499056d0bf94f8c735a

[JSC] Use DataIC for AccessCase https://bugs.webkit.org/show_bug.cgi?id=226072
Reviewed by Saam Barati and Filip Pizlo.

Source/JavaScriptCore:

This patch adds non-repatching IC for Baseline JIT in ARM64.
This does not work in non-ARM64 architectures (including X64) due to the use of link-register.

1. We add non-repatching IC, which is enabled only in Baseline due to performance reason. We are using the
   existing IC in DFG and FTL. Non-repatching includes fast-path, and slow-path's operation function.
2. We still keep InlineAccess in all tiers. Removing that causes 0.3 ~ 1.0% regression in Speedometer2. This
   means that we still need some repatching when we first introduce stubs.
3. We add a mechanism to share generated code stubs in non-repatching IC. Currently, getter / setter / custom
   accessors are excluded since their code relies on JSGlobalObject, CodeBlock etc. which are not included in
   AccessCase's data structure.
4. This patch still relies on that CodeBlock will be destroyed synchronously since we need to ensure that sharing-hash-table
   does not include already-dead JIT code stubs. We can fix it (e.g. annotating epoch to these stubs, bump them in
   finalizeUnconditionally), but we avoid doing that to prevent from further enlarging this patch. This patch is already
   significant in its size.
5. Added callOperation(Address) support in CCallHelpers, which can save the target in nonArgGPR0, and call it so that we can use
   Address including GPR which is also used for arguments.

Performance is neutral in JetStream2 and Speedometer2. But it offers the way to remove some code generation in Baseline.

* assembler/MacroAssemblerARM64E.h:
(JSC::MacroAssemblerARM64E::call):
* bytecode/AccessCase.cpp:
(JSC::AccessCase::create):
(JSC::AccessCase::createTransition):
(JSC::AccessCase::createDelete):
(JSC::AccessCase::createCheckPrivateBrand):
(JSC::AccessCase::createSetPrivateBrand):
(JSC::AccessCase::fromStructureStubInfo):

Source/JavaScriptCore/jit/JIT.h
@@ -294,6 +294,11 @@ namespace JSC {
             return functionCall;
         }
 
+        void appendCall(Address function)
+        {
+            call(function, OperationPtrTag);
+        }
+
 #if OS(WINDOWS) && CPU(X86_64)
         Call appendCallWithSlowPathReturnType(const FunctionPtr<CFunctionPtrTag> function)
         {
@@ -397,39 +402,39 @@ namespace JSC {
         // Property is int-checked and zero extended. Base is cell checked.
         // Structure is already profiled. Returns the slow cases. Fall-through
         // case contains result in regT0, and it is not yet profiled.
-        JumpList emitInt32Load(const Instruction* instruction, PatchableJump& badType) { return emitContiguousLoad(instruction, badType, Int32Shape); }
-        JumpList emitDoubleLoad(const Instruction*, PatchableJump& badType);
-        JumpList emitContiguousLoad(const Instruction*, PatchableJump& badType, IndexingType expectedShape = ContiguousShape);
-        JumpList emitArrayStorageLoad(const Instruction*, PatchableJump& badType);
-        JumpList emitLoadForArrayMode(const Instruction*, JITArrayMode, PatchableJump& badType);
+        JumpList emitInt32Load(const Instruction* instruction, PatchableJump& badType, ByValInfo* byValInfo) { return emitContiguousLoad(instruction, badType, byValInfo, Int32Shape); }
+        JumpList emitDoubleLoad(const Instruction*, PatchableJump& badType, ByValInfo*);
+        JumpList emitContiguousLoad(const Instruction*, PatchableJump& badType, ByValInfo*, IndexingType expectedShape = ContiguousShape);
+        JumpList emitArrayStorageLoad(const Instruction*, PatchableJump& badType, ByValInfo*);
+        JumpList emitLoadForArrayMode(const Instruction*, JITArrayMode, PatchableJump& badType, ByValInfo*);
 
         // Property is in regT1, base is in regT0. regT2 contains indecing type.
         // The value to store is not yet loaded. Property is int-checked and
         // zero-extended. Base is cell checked. Structure is already profiled.
         // returns the slow cases.
         template<typename Op>
-        JumpList emitInt32PutByVal(Op bytecode, PatchableJump& badType)
+        JumpList emitInt32PutByVal(Op bytecode, PatchableJump& badType, ByValInfo* byValInfo)
         {
-            return emitGenericContiguousPutByVal(bytecode, badType, Int32Shape);
+            return emitGenericContiguousPutByVal(bytecode, badType, byValInfo, Int32Shape);
         }
         template<typename Op>
-        JumpList emitDoublePutByVal(Op bytecode, PatchableJump& badType)
+        JumpList emitDoublePutByVal(Op bytecode, PatchableJump& badType, ByValInfo* byValInfo)
         {
-            return emitGenericContiguousPutByVal(bytecode, badType, DoubleShape);
+            return emitGenericContiguousPutByVal(bytecode, badType, byValInfo, DoubleShape);
         }
         template<typename Op>
-        JumpList emitContiguousPutByVal(Op bytecode, PatchableJump& badType)
+        JumpList emitContiguousPutByVal(Op bytecode, PatchableJump& badType, ByValInfo* byValInfo)
         {
-            return emitGenericContiguousPutByVal(bytecode, badType);
+            return emitGenericContiguousPutByVal(bytecode, badType, byValInfo);
         }
         template<typename Op>
-        JumpList emitGenericContiguousPutByVal(Op, PatchableJump& badType, IndexingType indexingShape = ContiguousShape);
+        JumpList emitGenericContiguousPutByVal(Op, PatchableJump& badType, ByValInfo*, IndexingType indexingShape = ContiguousShape);
         template<typename Op>
-        JumpList emitArrayStoragePutByVal(Op, PatchableJump& badType);
+        JumpList emitArrayStoragePutByVal(Op, PatchableJump& badType, ByValInfo*);
         template<typename Op>
-        JumpList emitIntTypedArrayPutByVal(Op, PatchableJump& badType, TypedArrayType);
+        JumpList emitIntTypedArrayPutByVal(Op, PatchableJump& badType, ByValInfo*, TypedArrayType);
         template<typename Op>
-        JumpList emitFloatTypedArrayPutByVal(Op, PatchableJump& badType, TypedArrayType);
+        JumpList emitFloatTypedArrayPutByVal(Op, PatchableJump& badType, ByValInfo*, TypedArrayType);
 
         template<typename Op>
         ECMAMode ecmaMode(Op);
@@ -901,13 +906,17 @@ namespace JSC {
         }
 
         MacroAssembler::Call appendCallWithExceptionCheck(const FunctionPtr<CFunctionPtrTag>);
+        void appendCallWithExceptionCheck(Address);
 #if OS(WINDOWS) && CPU(X86_64)
         MacroAssembler::Call appendCallWithExceptionCheckAndSlowPathReturnType(const FunctionPtr<CFunctionPtrTag>);
 #endif
         MacroAssembler::Call appendCallWithCallFrameRollbackOnException(const FunctionPtr<CFunctionPtrTag>);
         MacroAssembler::Call appendCallWithExceptionCheckSetJSValueResult(const FunctionPtr<CFunctionPtrTag>, VirtualRegister result);
+        void appendCallWithExceptionCheckSetJSValueResult(Address, VirtualRegister result);
         template<typename Metadata>
         MacroAssembler::Call appendCallWithExceptionCheckSetJSValueResultWithProfile(Metadata&, const FunctionPtr<CFunctionPtrTag>, VirtualRegister result);
+        template<typename Metadata>
+        void appendCallWithExceptionCheckSetJSValueResultWithProfile(Metadata&, Address, VirtualRegister result);
         
         template<typename OperationType, typename... Args>
         std::enable_if_t<FunctionTraits<OperationType>::hasResult, MacroAssembler::Call>
@@ -915,6 +924,14 @@ namespace JSC {
         {
             setupArguments<OperationType>(args...);
             return appendCallWithExceptionCheckSetJSValueResult(operation, result);
+        }
+
+        template<typename OperationType, typename... Args>
+        std::enable_if_t<FunctionTraits<OperationType>::hasResult, void>
+        callOperation(Address target, VirtualRegister result, Args... args)
+        {
+            setupArgumentsForIndirectCall<OperationType>(target, args...);
+            return appendCallWithExceptionCheckSetJSValueResult(Address(GPRInfo::nonArgGPR0, target.offset), result);
         }
 
 #if OS(WINDOWS) && CPU(X86_64)
@@ -937,12 +954,30 @@ namespace JSC {
                 return appendCallWithExceptionCheck(operation);
             return appendCallWithExceptionCheckAndSlowPathReturnType(operation);
         }
+
+        template<typename OperationType, typename... Args>
+        void callOperation(Address target, Args... args)
+        {
+            setupArgumentsForIndirectCall<OperationType>(target, args...);
+            // x64 Windows cannot use standard call when the return type is larger than 64 bits.
+            if constexpr (is64BitType<typename FunctionTraits<OperationType>::ResultType>::value)
+                appendCallWithExceptionCheck(Address(GPRInfo::nonArgGPR0, target.offset));
+            else
+                appendCallWithExceptionCheckAndSlowPathReturnType(Address(GPRInfo::nonArgGPR0, target.offset));
+        }
 #else // OS(WINDOWS) && CPU(X86_64)
         template<typename OperationType, typename... Args>
         MacroAssembler::Call callOperation(OperationType operation, Args... args)
         {
             setupArguments<OperationType>(args...);
             return appendCallWithExceptionCheck(operation);
+        }
+
+        template<typename OperationType, typename... Args>
+        void callOperation(Address target, Args... args)
+        {
+            setupArgumentsForIndirectCall<OperationType>(target, args...);
+            appendCallWithExceptionCheck(Address(GPRInfo::nonArgGPR0, target.offset));
         }
 #endif // OS(WINDOWS) && CPU(X86_64)
 
@@ -952,6 +987,14 @@ namespace JSC {
         {
             setupArguments<OperationType>(args...);
             return appendCallWithExceptionCheckSetJSValueResultWithProfile(metadata, operation, result);
+        }
+
+        template<typename OperationType, typename Metadata, typename... Args>
+        std::enable_if_t<FunctionTraits<OperationType>::hasResult, void>
+        callOperationWithProfile(Metadata& metadata, Address target, VirtualRegister result, Args... args)
+        {
+            setupArgumentsForIndirectCall<OperationType>(target, args...);
+            return appendCallWithExceptionCheckSetJSValueResultWithProfile(metadata, Address(GPRInfo::nonArgGPR0, target.offset), result);
         }
 
         template<typename OperationType, typename... Args>

commit 433e306d194afbb7d1c20ddf4cf7e78403021294

clang-cl: JIT.h(966,67): error: no viable conversion from 'JSC::AbstractMacroAssembler<JSC::X86Assembler>::Address' to 'FunctionPtr<CFunctionPtrTag>' https://bugs.webkit.org/show_bug.cgi?id=226850
Reviewed by Yusuke Suzuki.

WinCairo clang-cl builds got broken since r278656 (Bug 226072).

* jit/JIT.h: Use the common callOperation(Address, Args...) for
x64 Windows. Added static_assert to check the return type.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@278687 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jit/JIT.h
@@ -954,17 +954,6 @@ namespace JSC {
                 return appendCallWithExceptionCheck(operation);
             return appendCallWithExceptionCheckAndSlowPathReturnType(operation);
         }
-
-        template<typename OperationType, typename... Args>
-        void callOperation(Address target, Args... args)
-        {
-            setupArgumentsForIndirectCall<OperationType>(target, args...);
-            // x64 Windows cannot use standard call when the return type is larger than 64 bits.
-            if constexpr (is64BitType<typename FunctionTraits<OperationType>::ResultType>::value)
-                appendCallWithExceptionCheck(Address(GPRInfo::nonArgGPR0, target.offset));
-            else
-                appendCallWithExceptionCheckAndSlowPathReturnType(Address(GPRInfo::nonArgGPR0, target.offset));
-        }
 #else // OS(WINDOWS) && CPU(X86_64)
         template<typename OperationType, typename... Args>
         MacroAssembler::Call callOperation(OperationType operation, Args... args)
@@ -972,14 +961,18 @@ namespace JSC {
             setupArguments<OperationType>(args...);
             return appendCallWithExceptionCheck(operation);
         }
+#endif // OS(WINDOWS) && CPU(X86_64)
 
         template<typename OperationType, typename... Args>
         void callOperation(Address target, Args... args)
         {
+#if OS(WINDOWS) && CPU(X86_64)
+            // x64 Windows cannot use standard call when the return type is larger than 64 bits.
+            static_assert(is64BitType<typename FunctionTraits<OperationType>::ResultType>::value);
+#endif
             setupArgumentsForIndirectCall<OperationType>(target, args...);
             appendCallWithExceptionCheck(Address(GPRInfo::nonArgGPR0, target.offset));
         }
-#endif // OS(WINDOWS) && CPU(X86_64)
 
         template<typename Metadata, typename OperationType, typename... Args>
         std::enable_if_t<FunctionTraits<OperationType>::hasResult, MacroAssembler::Call>