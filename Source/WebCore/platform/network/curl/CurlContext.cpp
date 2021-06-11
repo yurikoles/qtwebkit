yurikoles/qtwebkit

commit 77377cdbc050ef52a18b302782ee2e7f101d4ec7

Fix WPT test resource-timing/cross-origin-redirects.html https://bugs.webkit.org/show_bug.cgi?id=190523 <rdar://45227788>
Reviewed by Brady Eidson.

LayoutTests/imported/w3c:

* web-platform-tests/navigation-timing/nav2_test_redirect_server-expected.txt:
* web-platform-tests/navigation-timing/nav2_test_redirect_server.html:
Sometimes, with our use of Performance::reduceTimeResolution, the event times are equal to each other.
Allow this.
* web-platform-tests/resource-timing/cross-origin-redirects-expected.txt: Added.
* web-platform-tests/resource-timing/cross-origin-redirects.html: Added.
* web-platform-tests/resource-timing/resources/entry-invariants.js: Added.
(const.assert_all_equal_):
(const.assert_ordered_):
(const.assert_zeroed_):
(const.assert_not_negative_):
(const.assert_positive_):
(const.attribute_test_internal):
(async const):
(const.attribute_test_with_validator):
* web-platform-tests/resource-timing/resources/resource-loaders.js: Added.
(const.load.font.path.return.document.fonts.ready.then):
(const.load.stylesheet.async path):
(const.load.iframe.async path):
(const.load.script.async path):
(const.load.xhr_sync.async path):

Source/WebCore:

Test: imported/w3c/web-platform-tests/resource-timing/cross-origin-redirects.html

This test took more to fix than I thought it would.  We used to collect our timing data
from various sources into various structures, mostly based on Google's work from 2010 with
a few things tacked on to it.  The timing specifications have changed considerably in the
last 11 years, and so should the shape of the structures we put the data into.

We used to have a LoadTiming structure in various places that claimed to contain the redirect times,
but markRedirectStart and markRedirectEnd were never called.  Instead we tried to collect this data
from the SubResourceLoader and DocumentLoader reporting redirects in the web process.  This wasn't great.

Source/WebCore/platform/network/curl/CurlContext.cpp
@@ -760,7 +760,7 @@ std::optional<long> CurlHandle::getHttpVersion()
     return version;
 }
 
-std::optional<NetworkLoadMetrics> CurlHandle::getNetworkLoadMetrics(const WTF::Seconds& domainLookupStart)
+std::optional<NetworkLoadMetrics> CurlHandle::getNetworkLoadMetrics(MonotonicTime startTime)
 {
     double nameLookup = 0.0;
     double connect = 0.0;
@@ -793,18 +793,18 @@ std::optional<NetworkLoadMetrics> CurlHandle::getNetworkLoadMetrics(const WTF::S
 
     NetworkLoadMetrics networkLoadMetrics;
 
-    networkLoadMetrics.domainLookupStart = domainLookupStart;
-    networkLoadMetrics.domainLookupEnd = domainLookupStart + Seconds(nameLookup);
-    networkLoadMetrics.connectStart = domainLookupStart + Seconds(nameLookup);
-    networkLoadMetrics.connectEnd = domainLookupStart + Seconds(connect);
+    networkLoadMetrics.domainLookupStart = startTime;
+    networkLoadMetrics.domainLookupEnd = startTime + Seconds(nameLookup);
+    networkLoadMetrics.connectStart = networkLoadMetrics.domainLookupEnd;
+    networkLoadMetrics.connectEnd = startTime + Seconds(connect);
 
     if (appConnect > 0.0) {
-        networkLoadMetrics.secureConnectionStart = domainLookupStart + Seconds(connect);
-        networkLoadMetrics.connectEnd = domainLookupStart + Seconds(appConnect);
+        networkLoadMetrics.secureConnectionStart = networkLoadMetrics.connectEnd;
+        networkLoadMetrics.connectEnd = startTime + Seconds(appConnect);
     }
 
     networkLoadMetrics.requestStart = networkLoadMetrics.connectEnd;
-    networkLoadMetrics.responseStart = domainLookupStart + Seconds(startTransfer);
+    networkLoadMetrics.responseStart = startTime + Seconds(startTransfer);
 
     if (version == CURL_HTTP_VERSION_1_0)
         networkLoadMetrics.protocol = httpVersion10;

commit 352c0c6da06d96ca155c6918d7944a955087612d

[REGRESSION][Curl] Network::ResourceTiming are broken after r278391 https://bugs.webkit.org/show_bug.cgi?id=226901
Reviewed by Fujii Hironori.

Initialize fetchStart with startTime in Curl.

No new tests.

* platform/network/curl/CurlContext.cpp:
(WebCore::CurlHandle::getNetworkLoadMetrics):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@278752 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/curl/CurlContext.cpp
@@ -793,6 +793,7 @@ std::optional<NetworkLoadMetrics> CurlHandle::getNetworkLoadMetrics(MonotonicTim
 
     NetworkLoadMetrics networkLoadMetrics;
 
+    networkLoadMetrics.fetchStart = startTime;
     networkLoadMetrics.domainLookupStart = startTime;
     networkLoadMetrics.domainLookupEnd = startTime + Seconds(nameLookup);
     networkLoadMetrics.connectStart = networkLoadMetrics.domainLookupEnd;