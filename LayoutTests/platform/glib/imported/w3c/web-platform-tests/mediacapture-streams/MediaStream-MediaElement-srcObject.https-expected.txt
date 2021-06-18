WebKit/WebKit-http

commit 140c4cb436860a03ae0259194ea6f0bd189211e2

[GPUP] Fix WebRTC layout test failures with GPU Process enabled https://bugs.webkit.org/show_bug.cgi?id=222505 <rdar://problem/74814573>
Unreviewed.


* gpu-process/TestExpectations:
Removed no longer valid expectations.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@273645 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/gpu-process/TestExpectations
@@ -84,10 +84,4 @@ webgl/max-active-contexts-gc.html [ Failure ]
 webgl/max-active-contexts-oldest-context-lost.html [ Failure ]
 webgl/max-active-contexts-webglcontextlost-prevent-default.html [ Timeout ]
 
-webrtc/audio-video-element-playing.html [ Failure ]
-webrtc/captureCanvas-webrtc-software-h264-baseline.html [ Timeout ]
-webrtc/captureCanvas-webrtc-software-h264-high.html [ Timeout ]
-webrtc/h264-baseline.html [ Failure ]
-webrtc/h264-high.html [ Failure ]
-webrtc/video-rotation-no-cvo.html [ Timeout ]
 imported/w3c/web-platform-tests/webrtc/RTCRtpSender-replaceTrack.https.html [ Timeout ]

yurikoles/qtwebkit

commit 75f97fe6afb3071506961e3f17c717c36bc758fc

[GStreamer] imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https.html is failing since r273645 https://bugs.webkit.org/show_bug.cgi?id=223508
Patch by Philippe Normand <pnormand@igalia.com> on 2021-06-18
Reviewed by Xabier Rodriguez-Calvar.

Source/WebCore:

Per spec, prevent seeks, playback rate changes and preload state updates on media elements backed by
the GStreamer MediaStream source element.

* platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
(WebCore::MediaPlayerPrivateGStreamer::seek):
(WebCore::MediaPlayerPrivateGStreamer::updatePlaybackRate):
(WebCore::MediaPlayerPrivateGStreamer::setPreload):
(WebCore::MediaPlayerPrivateGStreamer::maxMediaTimeSeekable const):

LayoutTests:

Unflag and add glib baseline for test now passing.

* platform/glib/TestExpectations:
* platform/glib/imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https-expected.txt: Added.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@279034 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -467,6 +467,11 @@ void MediaPlayerPrivateGStreamer::seek(const MediaTime& mediaTime)
     if (!m_pipeline || m_didErrorOccur)
         return;
 
+#if ENABLE(MEDIA_STREAM)
+    if (WEBKIT_IS_MEDIA_STREAM_SRC(m_source.get()))
+        return;
+#endif
+
     GST_INFO_OBJECT(pipeline(), "[Seek] seek attempt to %s", toString(mediaTime).utf8().data());
 
     // Avoid useless seeking.
@@ -520,6 +525,11 @@ void MediaPlayerPrivateGStreamer::seek(const MediaTime& mediaTime)
 
 void MediaPlayerPrivateGStreamer::updatePlaybackRate()
 {
+#if ENABLE(MEDIA_STREAM)
+    if (WEBKIT_IS_MEDIA_STREAM_SRC(m_source.get()))
+        return;
+#endif
+
     if (!m_isChangingRate)
         return;
 
@@ -638,6 +648,11 @@ void MediaPlayerPrivateGStreamer::setPreservesPitch(bool preservesPitch)
 
 void MediaPlayerPrivateGStreamer::setPreload(MediaPlayer::Preload preload)
 {
+#if ENABLE(MEDIA_STREAM)
+    if (WEBKIT_IS_MEDIA_STREAM_SRC(m_source.get()))
+        return;
+#endif
+
     GST_DEBUG_OBJECT(pipeline(), "Setting preload to %s", convertEnumerationToString(preload).utf8().data());
     if (preload == MediaPlayer::Preload::Auto && m_isLiveStream)
         return;
@@ -694,6 +709,11 @@ MediaTime MediaPlayerPrivateGStreamer::maxMediaTimeSeekable() const
 
     if (m_isLiveStream)
         return MediaTime::zeroTime();
+
+#if ENABLE(MEDIA_STREAM)
+    if (WEBKIT_IS_MEDIA_STREAM_SRC(m_source.get()))
+        return MediaTime::zeroTime();
+#endif
 
     MediaTime duration = durationMediaTime();
     GST_DEBUG_OBJECT(pipeline(), "maxMediaTimeSeekable, duration: %s", toString(duration).utf8().data());

LayoutTests/platform/glib/TestExpectations
@@ -1132,7 +1132,6 @@ webkit.org/b/224074 webrtc/concurrentVideoPlayback.html [ Timeout Pass ]
 webkit.org/b/206656 imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-removetrack.https.html [ Crash Failure Timeout ]
 webkit.org/b/206656 imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-preload-none-manual.https.html [ Crash Failure Pass ]
 webkit.org/b/210385 imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-firstframe.https.html [ Failure Crash Pass ]
-webkit.org/b/223508 imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https.html [ Failure ]
 
 #////////////////////////////////////////////////////////////////////////////////////////
 # End of WebRTC-related bugs

LayoutTests/platform/glib/imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https-expected.txt
@@ -1,0 +1,28 @@
+When prompted, accept to share your video stream.
+
+Description
+
+This test checks that the MediaStream object returned by the success callback in getUserMedia can be properly assigned to a video element via the srcObject attribute.
+
+
+
+Harness Error (TIMEOUT), message = null
+
+PASS Tests that a MediaStream can be assigned to a video element with srcObject
+PASS Tests that a MediaStream assigned to a video element is not seekable
+PASS Tests that a MediaStream assigned to a video element is in readyState HAVE_NOTHING initially
+TIMEOUT Tests that a MediaStream assigned to a video element has expected duration Test timed out
+NOTRUN Tests that a video element with a MediaStream assigned is not preloaded
+NOTRUN Tests that a video element with a MediaStream assigned ignores playbackRate attributes (defaultPlaybackRate is identical)
+NOTRUN Tests that a video element with a MediaStream assigned ignores playbackRate attributes (defaultPlaybackRate is different)
+NOTRUN Tests that a media element with an assigned MediaStream reports the played attribute as expected
+NOTRUN Tests that a media element with an assigned MediaStream reports the currentTime attribute as expected
+NOTRUN Tests that a media element with an assigned MediaStream starts its timeline at 0 regardless of when the MediaStream was created
+NOTRUN Tests that a media element with an assigned MediaStream does not advance currentTime while paused
+NOTRUN Tests that a media element with an assigned MediaStream does not start advancing currentTime until potentially playing
+NOTRUN Tests that the loop attribute has no effect on a media element with an assigned MediaStream
+NOTRUN Tests that a media element with an assigned MediaStream ends when the MediaStream becomes inactive through tracks ending
+NOTRUN Tests that an audio element with an assigned MediaStream ends when the MediaStream becomes inaudible through audio tracks ending
+NOTRUN Tests that a media element with an assigned MediaStream ends when the MediaStream becomes inactive through track removal
+NOTRUN Tests that an audio element with an assigned MediaStream ends when the MediaStream becomes inaudible through track removal
+