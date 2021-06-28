yurikoles/qtwebkit

commit 3a7af1997d5f7cff557d7681f8d273fc72a45d3d

Using a video as a source for a WebGL texture is slow and hangs on iOS 15 https://bugs.webkit.org/show_bug.cgi?id=227081
Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2021-06-24
Reviewed by Eric Carlson.

Source/WebCore:

Video frame transfer from GPU process to Web process would fail when
WebGL tried to copy the video frame to a texture. The failure was
artificial, due to use of WebCore::IOSurface as a unneeeded helper
to convert CVPixelBufferRef to cross-process object.

This would cause performance regression when WebGL would
fall back to software conversion fallback. This callback did work
as it obtains the image through "get last frame as native image"
API.

* platform/cocoa/CoreVideoSoftLink.cpp:
* platform/graphics/cocoa/IOSurface.h:
* platform/graphics/cocoa/IOSurface.mm:
Remove CVPixelBufferRef -> WebCore::IOSurface conversion function.
Remove WebCore::IOSurface -> CVPixelBufferRef conversion function.
These were used in WebKit IPC serialization for CVPixelBufferRef.
However, this is incorrect behavior as CVPixelBufferRef
is not always compatible with WebCore::IOSurface. IOSurfaceRef
has always a destination color space. This would be missing from
CVPixelBufferRef. The code is moved to the IPC serialization
functions.

Source/WebKit:

In current form, media is decoded in GPU process and WebGL is processed
in Web process.

Video frame transfer from GPU process to Web process would fail when
WebGL tried to copy the video frame to a texture. The failure was
artificial, due to use of WebCore::IOSurface as a unneeeded helper
to convert CVPixelBufferRef to a cross-process object.

The failure would occur when AVPlayerItemVideoOutput
would generate a CVPixelBufferRef without color space information. Creating
WebCore::IOSurface out of this would fail. WebCore::IOSurface
has always a color space. The failure is not significant, as WebCore::IOSurface
was only used as a convenience.

This would cause performance regression when WebGL would

Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.mm
@@ -66,6 +66,10 @@
 #import <pal/cocoa/DataDetectorsCoreSoftLink.h>
 #endif
 
+#if USE(AVFOUNDATION)
+#import <WebCore/CoreVideoSoftLink.h>
+#endif
+
 namespace IPC {
 
 void ArgumentCoder<WebCore::AttributedString>::encode(Encoder& encoder, const WebCore::AttributedString& attributedString)
@@ -731,5 +735,42 @@ bool ArgumentCoder<WebCore::TextRecognitionDataDetector>::decodePlatformData(Dec
 }
 
 #endif // ENABLE(IMAGE_ANALYSIS) && ENABLE(DATA_DETECTION)
+
+#if USE(AVFOUNDATION)
+
+void ArgumentCoder<RetainPtr<CVPixelBufferRef>>::encode(Encoder& encoder, const RetainPtr<CVPixelBufferRef>& pixelBuffer)
+{
+    // Use IOSurface as the means to transfer CVPixelBufferRef.
+    MachSendRight sendRight;
+    if (pixelBuffer) {
+        if (auto surface = WebCore::CVPixelBufferGetIOSurface(pixelBuffer.get()))
+            sendRight = MachSendRight::adopt(IOSurfaceCreateMachPort(surface));
+    }
+    encoder << WTFMove(sendRight);
+}
+
+std::optional<RetainPtr<CVPixelBufferRef>> ArgumentCoder<RetainPtr<CVPixelBufferRef>>::decode(Decoder& decoder)
+{
+    std::optional<MachSendRight> sendRight;
+    decoder >> sendRight;
+    if (!sendRight)
+        return std::nullopt;
+    RetainPtr<CVPixelBufferRef> pixelBuffer;
+    if (!*sendRight)
+        return pixelBuffer;
+    {
+        auto surface = adoptCF(IOSurfaceLookupFromMachPort(sendRight->sendRight()));
+        if (!surface)
+            return std::nullopt;
+        CVPixelBufferRef rawBuffer = nullptr;
+        auto status = WebCore::CVPixelBufferCreateWithIOSurface(kCFAllocatorDefault, surface.get(), nullptr, &rawBuffer);
+        if (status != noErr || !rawBuffer)
+            return std::nullopt;
+        pixelBuffer = adoptCF(rawBuffer);
+    }
+    return pixelBuffer;
+}
+
+#endif
 
 } // namespace IPC

Source/WebCore/platform/cocoa/CoreVideoSoftLink.cpp
@@ -45,7 +45,7 @@ SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferLockBaseAddress, 
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferUnlockBaseAddress, CVReturn, (CVPixelBufferRef pixelBuffer, CVOptionFlags lockFlags), (pixelBuffer, lockFlags))
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferPoolCreate, CVReturn,(CFAllocatorRef allocator, CFDictionaryRef poolAttributes, CFDictionaryRef pixelBufferAttributes, CVPixelBufferPoolRef* poolOut), (allocator, poolAttributes, pixelBufferAttributes, poolOut))
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferPoolCreatePixelBuffer, CVReturn, (CFAllocatorRef allocator, CVPixelBufferPoolRef pixelBufferPool, CVPixelBufferRef* pixelBufferOut), (allocator, pixelBufferPool, pixelBufferOut))
-SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferGetIOSurface, IOSurfaceRef, (CVPixelBufferRef pixelBuffer), (pixelBuffer))
+SOFT_LINK_FUNCTION_FOR_SOURCE_WITH_EXPORT(WebCore, CoreVideo, CVPixelBufferGetIOSurface, IOSurfaceRef, (CVPixelBufferRef pixelBuffer), (pixelBuffer), WEBCORE_EXPORT)
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVImageBufferGetColorSpace, CGColorSpaceRef, (CVImageBufferRef pixelBuffer), (pixelBuffer))
 
 SOFT_LINK_CONSTANT_FOR_SOURCE_WITH_EXPORT(WebCore, CoreVideo, kCVPixelBufferPixelFormatTypeKey, CFStringRef, WEBCORE_EXPORT)
@@ -98,5 +98,5 @@ SOFT_LINK_CONSTANT_FOR_SOURCE(WebCore, CoreVideo, kCVPixelBufferOpenGLCompatibil
 
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferCreate, CVReturn, (CFAllocatorRef allocator, size_t width, size_t height, OSType pixelFormatType, CFDictionaryRef pixelBufferAttributes, CVPixelBufferRef *pixelBufferOut), (allocator, width, height, pixelFormatType, pixelBufferAttributes, pixelBufferOut))
 SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferCreateWithBytes, CVReturn, (CFAllocatorRef allocator, size_t width, size_t height, OSType pixelFormatType, void* data, size_t bytesPerRow, void (*releaseCallback)(void*, const void*), void* releasePointer, CFDictionaryRef pixelBufferAttributes, CVPixelBufferRef *pixelBufferOut), (allocator, width, height, pixelFormatType, data, bytesPerRow, releaseCallback, releasePointer, pixelBufferAttributes, pixelBufferOut))
-SOFT_LINK_FUNCTION_FOR_SOURCE(WebCore, CoreVideo, CVPixelBufferCreateWithIOSurface, CVReturn, (CFAllocatorRef allocator, IOSurfaceRef surface, CFDictionaryRef pixelBufferAttributes, CVPixelBufferRef * pixelBufferOut), (allocator, surface, pixelBufferAttributes, pixelBufferOut))
+SOFT_LINK_FUNCTION_FOR_SOURCE_WITH_EXPORT(WebCore, CoreVideo, CVPixelBufferCreateWithIOSurface, CVReturn, (CFAllocatorRef allocator, IOSurfaceRef surface, CFDictionaryRef pixelBufferAttributes, CVPixelBufferRef * pixelBufferOut), (allocator, surface, pixelBufferAttributes, pixelBufferOut), WEBCORE_EXPORT)
 

Source/WebCore/platform/graphics/cocoa/IOSurface.h
@@ -36,8 +36,6 @@
 #define HAVE_IOSURFACE_RGB10 1
 #endif
 
-using CVPixelBufferRef = struct __CVBuffer*;
-
 namespace WTF {
 class MachSendRight;
 class TextStream;
@@ -104,7 +102,6 @@ public:
     WEBCORE_EXPORT static std::unique_ptr<IOSurface> createFromSendRight(const WTF::MachSendRight&&, const DestinationColorSpace&);
     WEBCORE_EXPORT static std::unique_ptr<IOSurface> createFromSurface(IOSurfaceRef, const DestinationColorSpace&);
     WEBCORE_EXPORT static std::unique_ptr<IOSurface> createFromImage(CGImageRef);
-    WEBCORE_EXPORT static std::unique_ptr<IOSurface> createFromPixelBuffer(CVPixelBufferRef);
     
 #if USE(IOSURFACE_CANVAS_BACKING_STORE)
     static std::unique_ptr<IOSurface> createFromImageBuffer(RefPtr<ImageBuffer>);
@@ -123,8 +120,6 @@ public:
     // the surface, or an expensive GPU readback can result.
     WEBCORE_EXPORT RetainPtr<CGImageRef> createImage();
     WEBCORE_EXPORT static RetainPtr<CGImageRef> sinkIntoImage(std::unique_ptr<IOSurface>);
-
-    WEBCORE_EXPORT RetainPtr<CVPixelBufferRef> createPixelBuffer();
 
 #ifdef __OBJC__
     id asLayerContents() const { return (__bridge id)m_surface.get(); }

Source/WebCore/platform/graphics/cocoa/IOSurface.mm
@@ -105,22 +105,6 @@ std::unique_ptr<IOSurface> IOSurface::createFromImage(CGImageRef image)
     return surface;
 }
 
-std::unique_ptr<IOSurface> IOSurface::createFromPixelBuffer(CVPixelBufferRef pixelBuffer)
-{
-    if (!pixelBuffer)
-        return nullptr;
-
-    auto surface = CVPixelBufferGetIOSurface(pixelBuffer);
-    if (!surface)
-        return nullptr;
-
-    auto colorSpace = CVImageBufferGetColorSpace(pixelBuffer);
-    if (!colorSpace)
-        return nullptr;
-
-    return createFromSurface(surface, DestinationColorSpace { colorSpace });
-}
-
 void IOSurface::moveToPool(std::unique_ptr<IOSurface>&& surface)
 {
     IOSurfacePool::sharedPool().addSurface(WTFMove(surface));
@@ -298,16 +282,6 @@ RetainPtr<CGImageRef> IOSurface::createImage()
 RetainPtr<CGImageRef> IOSurface::sinkIntoImage(std::unique_ptr<IOSurface> surface)
 {
     return adoptCF(CGIOSurfaceContextCreateImageReference(surface->ensurePlatformContext()));
-}
-
-RetainPtr<CVPixelBufferRef> IOSurface::createPixelBuffer()
-{
-    CVPixelBufferRef rawBuffer = nullptr;
-    auto status = CVPixelBufferCreateWithIOSurface(kCFAllocatorDefault, m_surface.get(), nullptr, &rawBuffer);
-    if (status == noErr && rawBuffer)
-        return adoptCF(rawBuffer);
-
-    return nullptr;
 }
 
 void IOSurface::setContextSize(IntSize contextSize)

Source/WebKit/GPUProcess/media/RemoteMediaPlayerProxy.h
@@ -62,6 +62,10 @@
 #include "SharedRingBufferStorage.h"
 #endif
 
+#if USE(AVFOUNDATION)
+#include <wtf/RetainPtr.h>
+#endif
+
 namespace WTF {
 class MachSendRight;
 }
@@ -70,6 +74,10 @@ namespace WebCore {
 class AudioTrackPrivate;
 class VideoTrackPrivate;
 }
+
+#if USE(AVFOUNDATION)
+typedef struct __CVBuffer* CVPixelBufferRef;
+#endif
 
 namespace WebKit {
 
@@ -298,7 +306,9 @@ private:
 
 #if PLATFORM(COCOA)
     void nativeImageForCurrentTime(CompletionHandler<void(std::optional<WTF::MachSendRight>&&)>&&);
-    void pixelBufferForCurrentTime(CompletionHandler<void(std::optional<WTF::MachSendRight>&&)>&&);
+#endif
+#if USE(AVFOUNDATION)
+    void pixelBufferForCurrentTime(CompletionHandler<void(RetainPtr<CVPixelBufferRef>&&)>&&);
 #endif
 
 #if !RELEASE_LOG_DISABLED

Source/WebKit/GPUProcess/media/RemoteMediaPlayerProxy.messages.in
@@ -124,7 +124,9 @@ messages -> RemoteMediaPlayerProxy NotRefCounted {
 
 #if PLATFORM(COCOA)
     NativeImageForCurrentTime() -> (std::optional<MachSendRight> sendRight) Synchronous
-    PixelBufferForCurrentTime() -> (std::optional<MachSendRight> sendRight) Synchronous
+#endif
+#if USE(AVFOUNDATION)
+    PixelBufferForCurrentTime() -> (RetainPtr<CVPixelBufferRef> pixelBuffer) Synchronous
 #endif
 }
 

Source/WebKit/GPUProcess/media/cocoa/RemoteMediaPlayerProxyCocoa.mm
@@ -107,30 +107,12 @@ void RemoteMediaPlayerProxy::nativeImageForCurrentTime(CompletionHandler<void(st
     completionHandler(surface->createSendRight());
 }
 
-void RemoteMediaPlayerProxy::pixelBufferForCurrentTime(CompletionHandler<void(std::optional<WTF::MachSendRight>&&)>&& completionHandler)
+void RemoteMediaPlayerProxy::pixelBufferForCurrentTime(CompletionHandler<void(RetainPtr<CVPixelBufferRef>&&)>&& completionHandler)
 {
-#if !USE(AVFOUNDATION)
-    completionHandler(std::nullopt);
-#else
-    if (!m_player) {
-        completionHandler(std::nullopt);
-        return;
-    }
-
-    auto pixelBuffer = m_player->pixelBufferForCurrentTime();
-    if (!pixelBuffer) {
-        completionHandler(std::nullopt);
-        return;
-    }
-
-    auto surface = WebCore::IOSurface::createFromPixelBuffer(pixelBuffer.get());
-    if (!surface) {
-        completionHandler(std::nullopt);
-        return;
-    }
-
-    completionHandler(surface->createSendRight());
-#endif
+    RetainPtr<CVPixelBufferRef> result;
+    if (m_player)
+        result = m_player->pixelBufferForCurrentTime();
+    completionHandler(WTFMove(result));
 }
 
 } // namespace WebKit

Source/WebKit/Scripts/webkit/messages.py
@@ -28,7 +28,7 @@ from webkit import parser
 from webkit.model import BUILTIN_ATTRIBUTE, ASYNC_ATTRIBUTE, SYNCHRONOUS_ATTRIBUTE, MAINTHREADCALLBACK_ATTRIBUTE, STREAM_ATTRIBUTE, WANTS_CONNECTION_ATTRIBUTE, MessageReceiver, Message
 
 _license_header = """/*
- * Copyright (C) 2010-2021 Apple Inc. All rights reserved.
+ * Copyright (C) 2021 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -264,6 +264,7 @@ def forward_declarations_for_namespace(namespace, kind_and_types):
 
 def types_that_cannot_be_forward_declared():
     return frozenset([
+        'CVPixelBufferRef',
         'IPC::DataReference',
         'IPC::FontReference',
         'IPC::Semaphore',
@@ -607,6 +608,7 @@ def class_template_headers(template_string):
         'std::pair': {'headers': ['<utility>'], 'argument_coder_headers': ['"ArgumentCoders.h"']},
         'IPC::ArrayReference': {'headers': ['"ArrayReference.h"'], 'argument_coder_headers': ['"ArgumentCoders.h"']},
         'RefPtr': {'headers': ['<wtf/RefCounted.h>'], 'argument_coder_headers': ['"ArgumentCoders.h"']},
+        'RetainPtr': {'headers': ['<wtf/RetainPtr.h>'], 'argument_coder_headers': ['"ArgumentCodersCF.h"']},
     }
 
     match = re.match('(?P<template_name>.+?)<(?P<parameter_string>.+)>', template_string)

commit 29247f38e641cc6fb3e4ce3e04e76b9e0c27d3b1

Possible build breakage after r279221 in some unified build configurations https://bugs.webkit.org/show_bug.cgi?id=227440
Unreviewed build fix.

Add missing #import <wtf/MachSendRight.h> since
the code use MachSendRight. The code happens to work on default configuration
due to unified build.

Patch by Kimmo Kinnunen <kkinnunen@apple.com> on 2021-06-28

* Shared/Cocoa/WebCoreArgumentCodersCocoa.mm:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@279328 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Shared/Cocoa/WebCoreArgumentCodersCocoa.mm
@@ -52,6 +52,10 @@
 #include <WebKitAdditions/WebCoreArgumentCodersCocoaAdditions.mm>
 #endif
 
+#if USE(AVFOUNDATION)
+#import <wtf/MachSendRight.h>
+#endif
+
 #if ENABLE(APPLE_PAY)
 #import "DataReference.h"
 #import <WebCore/PaymentAuthorizationStatus.h>