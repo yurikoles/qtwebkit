yurikoles/qtwebkit

commit e4468fb9322ec155948b44e446f87754ad802959

[WPE][GTK] SVN_REVISION drifting away if bots don't re-run cmake https://bugs.webkit.org/show_bug.cgi?id=228290
Patch by Philippe Normand <pnormand@igalia.com> on 2021-07-28
Reviewed by Michael Catanzaro.

.:

The CMake configure_file() is now used only tarball builds for files requiring information
about the build revision. For developer builds a custom target now takes care of keeping
those files up-to-date.

* Source/cmake/OptionsGTK.cmake:
* Source/cmake/OptionsWPE.cmake:

Source/JavaScriptCore:

* PlatformGTK.cmake: Expand ${BUILD_REVISION} with apply-build-revision-to-files.py for
non-tarball builds, or let CMake do it for tarball builds.
* javascriptcoregtk.pc.in: Switch revision var to ${} format, so that we can choose to
either let CMake expand it or do it ourselves.

Source/WebKit:

* PlatformGTK.cmake: Expand ${BUILD_REVISION} with apply-build-revision-to-files.py for
non-tarball builds, or let CMake do it for tarball builds.
* PlatformWPE.cmake: Ditto.
* UIProcess/API/glib/WebKitProtocolHandler.cpp:
(WebKit::WebKitProtocolHandler::handleGPU): Use generated BuildRevision.h header
* gtk/webkit2gtk-web-extension.pc.in: Switch revision var to ${} format, so that we can choose to
either let CMake expand it or do it ourselves.
* gtk/webkit2gtk.pc.in: Ditto.

Tools:

* MiniBrowser/gtk/main.c:
(main): Use generated BuildRevision.h header.
* glib/apply-build-revision-to-files.py: Added.
(main):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@280382 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/PlatformGTK.cmake
@@ -3,7 +3,18 @@ include(inspector/remote/GLib.cmake)
 
 set(JavaScriptCore_OUTPUT_NAME javascriptcoregtk-${WEBKITGTK_API_VERSION})
 
-configure_file(javascriptcoregtk.pc.in ${JavaScriptCore_PKGCONFIG_FILE} @ONLY)
+if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
+    configure_file(javascriptcoregtk.pc.in ${JavaScriptCore_PKGCONFIG_FILE} @ONLY)
+    add_custom_target(JavaScriptCore-build-revision
+        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${JavaScriptCore_PKGCONFIG_FILE}
+        DEPENDS ${JavaScriptCore_PKGCONFIG_FILE}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
+    list(APPEND JavaScriptCore_DEPENDENCIES
+        JavaScriptCore-build-revision
+    )
+else ()
+    configure_file(javascriptcoregtk.pc.in ${JavaScriptCore_PKGCONFIG_FILE})
+endif ()
 
 install(FILES "${CMAKE_BINARY_DIR}/Source/JavaScriptCore/javascriptcoregtk-${WEBKITGTK_API_VERSION}.pc"
         DESTINATION "${LIB_INSTALL_DIR}/pkgconfig"

Source/WebKit/PlatformGTK.cmake
@@ -11,8 +11,23 @@ file(MAKE_DIRECTORY ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/webkit2gtk-${WEBKITGTK_A
 file(MAKE_DIRECTORY ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/webkit2gtk-webextension)
 
 configure_file(UIProcess/API/gtk/WebKitVersion.h.in ${WebKit2Gtk_DERIVED_SOURCES_DIR}/webkit2/WebKitVersion.h)
-configure_file(gtk/webkit2gtk.pc.in ${WebKit2_PKGCONFIG_FILE} @ONLY)
-configure_file(gtk/webkit2gtk-web-extension.pc.in ${WebKit2WebExtension_PKGCONFIG_FILE} @ONLY)
+
+if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
+    configure_file(gtk/webkit2gtk.pc.in ${WebKit2_PKGCONFIG_FILE} @ONLY)
+    configure_file(gtk/webkit2gtk-web-extension.pc.in ${WebKit2WebExtension_PKGCONFIG_FILE} @ONLY)
+    configure_file(Shared/glib/BuildRevision.h.in ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h @ONLY)
+    add_custom_target(WebKit-build-revision
+        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h ${WebKit2_PKGCONFIG_FILE} ${WebKit2WebExtension_PKGCONFIG_FILE}
+        DEPENDS ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h ${WebKit2_PKGCONFIG_FILE} ${WebKit2WebExtension_PKGCONFIG_FILE}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
+    list(APPEND WebKit_DEPENDENCIES
+        WebKit-build-revision
+    )
+else ()
+    configure_file(gtk/webkit2gtk.pc.in ${WebKit2_PKGCONFIG_FILE})
+    configure_file(gtk/webkit2gtk-web-extension.pc.in ${WebKit2WebExtension_PKGCONFIG_FILE})
+    configure_file(Shared/glib/BuildRevision.h.in ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h)
+endif ()
 
 add_definitions(-DBUILDING_WEBKIT)
 add_definitions(-DWEBKIT2_COMPILATION)

Source/WebKit/PlatformWPE.cmake
@@ -18,6 +18,19 @@ configure_file(wpe/wpe-web-extension.pc.in ${WPEWebExtension_PKGCONFIG_FILE} @ON
 configure_file(wpe/wpe-webkit-uninstalled.pc.in ${CMAKE_BINARY_DIR}/wpe-webkit-${WPE_API_VERSION}-uninstalled.pc @ONLY)
 configure_file(wpe/wpe-web-extension-uninstalled.pc.in ${CMAKE_BINARY_DIR}/wpe-web-extension-${WPE_API_VERSION}-uninstalled.pc @ONLY)
 
+if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
+    configure_file(Shared/glib/BuildRevision.h.in ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h @ONLY)
+    add_custom_target(WebKit-build-revision
+        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h
+        DEPENDS ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
+    list(APPEND WebKit_DEPENDENCIES
+        WebKit-build-revision
+    )
+else ()
+    configure_file(Shared/glib/BuildRevision.h.in ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h)
+endif ()
+
 add_definitions(-DWEBKIT2_COMPILATION)
 
 add_definitions(-DLIBDIR="${LIB_INSTALL_DIR}")

Tools/glib/apply-build-revision-to-files.py
@@ -1,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2021 Igalia S.L.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+import os
+import sys
+
+top_level_directory = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, os.path.join(top_level_directory, "Tools", "Scripts"))
+
+from webkitpy.common.checkout.scm.detection import SCMDetector  # nopep8
+from webkitpy.common.system.executive import Executive  # nopep8
+from webkitpy.common.system.filesystem import FileSystem  # nopep8
+
+
+def main(args):
+    scm = SCMDetector(FileSystem(), Executive()).default_scm()
+    svn_revision = scm.head_svn_revision()
+    build_revision = f"r{svn_revision}"
+
+    for in_file in args:
+        with open(in_file) as fd:
+            data = fd.read().replace('${BUILD_REVISION}', build_revision)
+        with open(in_file, 'w') as fd:
+            fd.write(data)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))

Source/JavaScriptCore/javascriptcoregtk.pc.in
@@ -2,7 +2,7 @@ prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
 libdir=@LIB_INSTALL_DIR@
 includedir=${prefix}/include
-revision=@SVN_REVISION@
+revision=${BUILD_REVISION}
 
 Name: JavaScriptCoreGTK+
 Description: GTK+ version of the JavaScriptCore engine

Source/WebKit/Shared/glib/BuildRevision.h.in
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2021 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#define BUILD_REVISION "${BUILD_REVISION}"

Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
@@ -20,6 +20,7 @@
 #include "config.h"
 #include "WebKitProtocolHandler.h"
 
+#include "BuildRevision.h"
 #include "WebKitError.h"
 #include "WebKitVersion.h"
 #include "WebKitWebView.h"
@@ -202,7 +203,7 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request)
         "  <td><div class=\"titlename\">WebKit version</div></td>"
         "  <td>%s %d.%d.%d (%s)</td>"
         " </tbody></tr>",
-        webkitPortName(), WEBKIT_MAJOR_VERSION, WEBKIT_MINOR_VERSION, WEBKIT_MICRO_VERSION, SVN_REVISION);
+        webkitPortName(), WEBKIT_MAJOR_VERSION, WEBKIT_MINOR_VERSION, WEBKIT_MICRO_VERSION, BUILD_REVISION);
 
 #if OS(UNIX)
     struct utsname osName;

Source/WebKit/gtk/webkit2gtk-web-extension.pc.in
@@ -2,7 +2,7 @@ prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
 libdir=@LIB_INSTALL_DIR@
 includedir=${prefix}/include
-revision=@SVN_REVISION@
+revision=${BUILD_REVISION}
 
 Name: WebKitGTK web process extensions
 Description: Web content engine for GTK - web process extensions

commit 420736306c5467c3cb00d577b25bc2ee6504ace4

[WPE][GTK] build broken with python2 after r280382 https://bugs.webkit.org/show_bug.cgi?id=228629
Reviewed by Philippe Normand.

Source/JavaScriptCore:

Call {PYTHON_EXECUTABLE} instead of python3.

* PlatformGTK.cmake:

Source/WebKit:

Call {PYTHON_EXECUTABLE} instead of python3. Our build currently
supports both versions of python (2 and 3). If the user wants
to force a specific version it can do that via the CMake argument
-DPYTHON_EXECUTABLE=/path/to/python/interpreter

No new tests, is a build fix.

* PlatformGTK.cmake:
* PlatformWPE.cmake:

Tools:

Fix compatibility with python2.

* glib/apply-build-revision-to-files.py:
(main):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@280466 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/PlatformGTK.cmake
@@ -6,7 +6,7 @@ set(JavaScriptCore_OUTPUT_NAME javascriptcoregtk-${WEBKITGTK_API_VERSION})
 if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
     configure_file(javascriptcoregtk.pc.in ${JavaScriptCore_PKGCONFIG_FILE} @ONLY)
     add_custom_target(JavaScriptCore-build-revision
-        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${JavaScriptCore_PKGCONFIG_FILE}
+        ${PYTHON_EXECUTABLE} "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${JavaScriptCore_PKGCONFIG_FILE}
         DEPENDS ${JavaScriptCore_PKGCONFIG_FILE}
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
     list(APPEND JavaScriptCore_DEPENDENCIES

Source/WebKit/PlatformGTK.cmake
@@ -17,7 +17,7 @@ if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
     configure_file(gtk/webkit2gtk-web-extension.pc.in ${WebKit2WebExtension_PKGCONFIG_FILE} @ONLY)
     configure_file(Shared/glib/BuildRevision.h.in ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h @ONLY)
     add_custom_target(WebKit-build-revision
-        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h ${WebKit2_PKGCONFIG_FILE} ${WebKit2WebExtension_PKGCONFIG_FILE}
+        ${PYTHON_EXECUTABLE} "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h ${WebKit2_PKGCONFIG_FILE} ${WebKit2WebExtension_PKGCONFIG_FILE}
         DEPENDS ${WebKit2Gtk_FRAMEWORK_HEADERS_DIR}/BuildRevision.h ${WebKit2_PKGCONFIG_FILE} ${WebKit2WebExtension_PKGCONFIG_FILE}
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
     list(APPEND WebKit_DEPENDENCIES

Source/WebKit/PlatformWPE.cmake
@@ -21,7 +21,7 @@ configure_file(wpe/wpe-web-extension-uninstalled.pc.in ${CMAKE_BINARY_DIR}/wpe-w
 if (EXISTS "${TOOLS_DIR}/glib/apply-build-revision-to-files.py")
     configure_file(Shared/glib/BuildRevision.h.in ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h @ONLY)
     add_custom_target(WebKit-build-revision
-        python3 "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h
+        ${PYTHON_EXECUTABLE} "${TOOLS_DIR}/glib/apply-build-revision-to-files.py" ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h
         DEPENDS ${FORWARDING_HEADERS_WPE_DIR}/BuildRevision.h
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} VERBATIM)
     list(APPEND WebKit_DEPENDENCIES

Tools/glib/apply-build-revision-to-files.py
@@ -30,7 +30,7 @@ from webkitpy.common.system.filesystem import FileSystem  # nopep8
 def main(args):
     scm = SCMDetector(FileSystem(), Executive()).default_scm()
     svn_revision = scm.head_svn_revision()
-    build_revision = f"r{svn_revision}"
+    build_revision = "r{}".format(svn_revision)
 
     for in_file in args:
         with open(in_file) as fd: