yurikoles/qtwebkit

commit 5cc52e1c406b808898f1de8f2fc5c7c167c25aa0

Add an IPCMessages channel to log all IPC messages https://bugs.webkit.org/show_bug.cgi?id=229664 <rdar://problem/82511640>
Reviewed by Alex Christensen.

Source/WebCore:

Add a way to get the type of process we're in as a string, for
logging.

* platform/RuntimeApplicationChecks.cpp:
(WebCore::processType):
(WebCore::processTypeDescription):
* platform/RuntimeApplicationChecks.h:

Source/WebKit:

This adds a new IPCMessages log channel, to which all IPC message names,
arguments, and return values are logged. To avoid performance impacts
when not logging, these are only enabled in debug builds.

ValueOrDefault is used when writing out each message argument to (1)
avoid having to add operator<<(TextStream&) defintions for all
argument types right now, and (2) to alleviate the need to add such a
definition when adding new IPC messages. The string "..." is output
for types with no operator<< defined.

The remote and local process IDs are included in the log message. The
remote process ID is only readily accessible on OS(DARWIN), as we can
grab it from the XPC connection. But not all IPC::Connections are
created for XPC connections. For example, the Web -> GPU process
connection is created by the UI process on behalf of the Web process,
and the IPC::Connection::Identifier does not have an XPC service
handle in it. So for now, we omit the remote process ID. (We'd need
to thread it through messages like CreateGPUConnectionToWebProcess
to have it available.)

Since we need the IPC::Connection object for logging now, all of the
handleMessage* functions take a connection, and the messages.py code
generator is updated accordingly.

* Platform/IPC/HandleMessage.h:
(IPC::textStreamForLogging):
(IPC::logMessageImpl):
(IPC::logMessage):
(IPC::logReply):
(IPC::handleMessage):
(IPC::handleMessageWantsConnection):
(IPC::handleMessageSynchronous):
(IPC::handleMessageSynchronousWantsConnection):
(IPC::handleMessageAsync):
(IPC::handleMessageAsyncWantsConnection):

Source/WebKit/Platform/IPC/HandleMessage.h
@@ -27,13 +27,92 @@
 
 #include "ArgumentCoders.h"
 #include "DataReference.h"
+#include "Logging.h"
+#include "MessageArgumentDescriptions.h"
+#include "MessageNames.h"
 #include "StreamServerConnection.h"
+#include <WebCore/RuntimeApplicationChecks.h>
 #include <wtf/CompletionHandler.h>
+#include <wtf/ProcessID.h>
 #include <wtf/StdLibExtras.h>
 
 namespace IPC {
 
 class Connection;
+
+// IPC message logging. Only enabled in DEBUG builds.
+//
+// Message argument values appear as "..." if no operator<<(TextStream&) is
+// implemented for them.
+
+constexpr unsigned loggingContainerSizeLimit = 200;
+
+#if !LOG_DISABLED
+enum class ForReply : bool { No, Yes };
+
+inline TextStream textStreamForLogging(const Connection& connection, MessageName messageName, ForReply forReply)
+{
+    TextStream stream(TextStream::LineMode::SingleLine, { }, loggingContainerSizeLimit);
+    stream << '[';
+#if OS(DARWIN)
+    // The remote process ID is not available when the connection was not made
+    // for an XPC service, e.g. for the Web -> GPU process connection.
+    if (connection.remoteProcessID())
+        stream << connection.remoteProcessID() << ' ';
+#else
+    UNUSED_PARAM(connection);
+#endif
+    auto arrow = forReply == ForReply::Yes ? "<- " : "-> ";
+    stream << arrow << WebCore::processTypeDescription(WebCore::processType()) << ' ' << getCurrentProcessID() << "] " << description(messageName);
+    if (forReply == ForReply::Yes)
+        stream << " Reply";
+    return stream;
+}
+#endif
+
+template<typename ArgsTuple, size_t... ArgsIndex>
+void logMessageImpl(const Connection& connection, MessageName messageName, const ArgsTuple& args, std::index_sequence<ArgsIndex...>)
+{
+#if !LOG_DISABLED
+    auto stream = textStreamForLogging(connection, messageName, ForReply::No);
+
+    if (auto argumentDescriptions = messageArgumentDescriptions(messageName))
+        (stream.dumpProperty((*argumentDescriptions)[ArgsIndex].name, ValueOrEllipsis(std::get<ArgsIndex>(args))), ...);
+
+    LOG(IPCMessages, "%s", stream.release().utf8().data());
+#else
+    UNUSED_PARAM(connection);
+    UNUSED_PARAM(messageName);
+    UNUSED_PARAM(args);
+#endif
+}
+
+template<typename ArgsTuple, typename ArgsIndices = std::make_index_sequence<std::tuple_size<ArgsTuple>::value>>
+void logMessage(const Connection& connection, MessageName messageName, const ArgsTuple& args)
+{
+    logMessageImpl(connection, messageName, args, ArgsIndices());
+}
+
+template<typename... T>
+void logReply(const Connection& connection, MessageName messageName, const T&... args)
+{
+#if !LOG_DISABLED
+    if (!sizeof...(T))
+        return;
+
+    auto stream = textStreamForLogging(connection, messageName, ForReply::Yes);
+
+    unsigned argIndex = 0;
+    if (auto argumentDescriptions = messageReplyArgumentDescriptions(messageName))
+        (stream.dumpProperty((*argumentDescriptions)[argIndex++].name, ValueOrEllipsis(args)), ...);
+
+    LOG(IPCMessages, "%s", stream.release().utf8().data());
+#else
+    UNUSED_PARAM(connection);
+    UNUSED_PARAM(messageName);
+    (UNUSED_PARAM(args), ...);
+#endif
+}
 
 // Dispatch functions with no reply arguments.
 
@@ -109,23 +188,26 @@ struct CodingType<std::tuple<Ts...>> {
 };
 
 template<typename T, typename C, typename MF>
-void handleMessage(Decoder& decoder, C* object, MF function)
-{
-    std::optional<typename CodingType<typename T::Arguments>::Type> arguments;
-    decoder >> arguments;
-    if (UNLIKELY(!arguments))
-        return;
-
-    callMemberFunction(WTFMove(*arguments), object, function);
-}
-
-template<typename T, typename C, typename MF>
 void handleMessage(Connection& connection, Decoder& decoder, C* object, MF function)
 {
     std::optional<typename CodingType<typename T::Arguments>::Type> arguments;
     decoder >> arguments;
     if (UNLIKELY(!arguments))
         return;
+
+    logMessage(connection, T::name(), *arguments);
+    callMemberFunction(WTFMove(*arguments), object, function);
+}
+
+template<typename T, typename C, typename MF>
+void handleMessageWantsConnection(Connection& connection, Decoder& decoder, C* object, MF function)
+{
+    std::optional<typename CodingType<typename T::Arguments>::Type> arguments;
+    decoder >> arguments;
+    if (UNLIKELY(!arguments))
+        return;
+
+    logMessage(connection, T::name(), *arguments);
     callMemberFunction(connection, WTFMove(*arguments), object, function);
 }
 
@@ -138,8 +220,10 @@ bool handleMessageSynchronous(Connection& connection, Decoder& decoder, UniqueRe
         return false;
 
     typename T::DelayedReply completionHandler = [replyEncoder = WTFMove(replyEncoder), connection = makeRef(connection)] (auto&&... args) mutable {
+        logReply(connection, T::name(), args...);
         T::send(WTFMove(replyEncoder), WTFMove(connection), args...);
     };
+    logMessage(connection, T::name(), *arguments);
     callMemberFunction(WTFMove(*arguments), WTFMove(completionHandler), object, function);
     return true;
 }
@@ -153,8 +237,10 @@ bool handleMessageSynchronousWantsConnection(Connection& connection, Decoder& de
         return false;
     
     typename T::DelayedReply completionHandler = [replyEncoder = WTFMove(replyEncoder), connection = makeRef(connection)] (auto&&... args) mutable {
+        logReply(connection, T::name(), args...);
         T::send(WTFMove(replyEncoder), WTFMove(connection), args...);
     };
+    logMessage(connection, T::name(), *arguments);
     callMemberFunction(connection, WTFMove(*arguments), WTFMove(completionHandler), object, function);
     return true;
 }
@@ -172,8 +258,10 @@ void handleMessageSynchronous(StreamServerConnectionBase& connection, Decoder& d
         return;
 
     typename T::DelayedReply completionHandler = [syncRequestID, connection = makeRef(connection)] (auto&&... args) mutable {
+        logReply(connection->connection(), T::name(), args...);
         connection->sendSyncReply<T>(syncRequestID, args...);
     };
+    logMessage(connection.connection(), T::name(), *arguments);
     callMemberFunction(WTFMove(*arguments), WTFMove(completionHandler), object, function);
 }
 
@@ -192,8 +280,10 @@ void handleMessageAsync(Connection& connection, Decoder& decoder, C* object, MF 
 
     typename T::AsyncReply completionHandler = { [listenerID = *listenerID, connection = makeRef(connection)] (auto&&... args) mutable {
         auto encoder = makeUniqueRef<Encoder>(T::asyncMessageReplyName(), listenerID);
+        logReply(connection, T::name(), args...);
         T::send(WTFMove(encoder), WTFMove(connection), args...);
     }, T::callbackThread };
+    logMessage(connection, T::name(), *arguments);
     callMemberFunction(WTFMove(*arguments), WTFMove(completionHandler), object, function);
 }
 
@@ -212,8 +302,10 @@ void handleMessageAsyncWantsConnection(Connection& connection, Decoder& decoder,
 
     typename T::AsyncReply completionHandler = { [listenerID = *listenerID, connection = makeRef(connection)] (auto&&... args) mutable {
         auto encoder = makeUniqueRef<Encoder>(T::asyncMessageReplyName(), listenerID);
+        logReply(connection, T::name(), args...);
         T::send(WTFMove(encoder), WTFMove(connection), args...);
     }, T::callbackThread };
+    logMessage(connection, T::name(), *arguments);
     callMemberFunction(connection, WTFMove(*arguments), WTFMove(completionHandler), object, function);
 }
 

Source/WTF/wtf/text/TextStream.h
@@ -53,9 +53,10 @@ public:
     };
 
     enum class LineMode { SingleLine, MultipleLine };
-    TextStream(LineMode lineMode = LineMode::MultipleLine, OptionSet<Formatting> formattingFlags = { })
+    TextStream(LineMode lineMode = LineMode::MultipleLine, OptionSet<Formatting> formattingFlags = { }, unsigned containerSizeLimit = 0)
         : m_formattingFlags(formattingFlags)
         , m_multiLineMode(lineMode == LineMode::MultipleLine)
+        , m_containerSizeLimit(containerSizeLimit)
     {
     }
 
@@ -111,6 +112,8 @@ public:
     void decreaseIndent(int amount = 1) { m_indent -= amount; ASSERT(m_indent >= 0); }
 
     WTF_EXPORT_PRIVATE void writeIndent();
+
+    unsigned containerSizeLimit() const { return m_containerSizeLimit; }
 
     // Stream manipulators.
     TextStream& operator<<(TextStream& (*func)(TextStream&))
@@ -174,6 +177,7 @@ private:
     int m_indent { 0 };
     OptionSet<Formatting> m_formattingFlags;
     bool m_multiLineMode { true };
+    unsigned m_containerSizeLimit { 0 };
 };
 
 inline TextStream& indent(TextStream& ts)
@@ -223,12 +227,17 @@ TextStream& operator<<(TextStream& ts, const Vector<ItemType, inlineCapacity>& v
 {
     ts << "[";
 
-    unsigned size = vector.size();
-    for (unsigned i = 0; i < size; ++i) {
-        ts << vector[i];
-        if (i < size - 1)
+    unsigned count = 0;
+    for (const auto& value : vector) {
+        if (count)
             ts << ", ";
+        ts << value;
+        if (++count == ts.containerSizeLimit())
+            break;
     }
+
+    if (count != vector.size())
+        ts << ", ...";
 
     return ts << "]";
 }
@@ -262,13 +271,17 @@ TextStream& operator<<(TextStream& ts, const HashMap<KeyArg, MappedArg, HashArg,
 {
     ts << "{";
 
-    bool first = true;
+    unsigned count = 0;
     for (const auto& keyValuePair : map) {
-        ts << keyValuePair.key << ": " << keyValuePair.value;
-        if (!first)
+        if (count)
             ts << ", ";
-        first = false;
+        ts << keyValuePair.key << ": " << keyValuePair.value;
+        if (++count == ts.containerSizeLimit())
+            break;
     }
+
+    if (count != map.size())
+        ts << ", ...";
 
     return ts << "}";
 }
@@ -278,13 +291,17 @@ TextStream& operator<<(TextStream& ts, const HashSet<ValueArg, HashArg, TraitsAr
 {
     ts << "[";
 
-    bool first = true;
+    unsigned count = 0;
     for (const auto& item : set) {
-        ts << item;
-        if (!first)
+        if (count)
             ts << ", ";
-        first = false;
+        ts << item;
+        if (++count == ts.containerSizeLimit())
+            break;
     }
+
+    if (count != set.size())
+        ts << ", ...";
 
     return ts << "]";
 }
@@ -303,11 +320,63 @@ TextStream& operator<<(TextStream& ts, const OptionSet<Option>& options)
     return ts << "]";
 }
 
+template<typename, typename = void, typename = void, typename = void, typename = void, size_t = 0>
+struct supports_text_stream_insertion : std::false_type { };
+
+template<typename T>
+struct supports_text_stream_insertion<T, std::void_t<decltype(std::declval<TextStream&>() << std::declval<T>())>> : std::true_type { };
+
+template<typename ItemType, size_t inlineCapacity>
+struct supports_text_stream_insertion<Vector<ItemType, inlineCapacity>> : supports_text_stream_insertion<ItemType> { };
+
+template<typename ValueArg, typename HashArg, typename TraitsArg>
+struct supports_text_stream_insertion<HashSet<ValueArg, HashArg, TraitsArg>> : supports_text_stream_insertion<ValueArg> { };
+
+template<typename KeyArg, typename MappedArg, typename HashArg, typename KeyTraitsArg, typename MappedTraitsArg>
+struct supports_text_stream_insertion<HashMap<KeyArg, MappedArg, HashArg, KeyTraitsArg, MappedTraitsArg>> : std::conjunction<supports_text_stream_insertion<KeyArg>, supports_text_stream_insertion<MappedArg>> { };
+
+template<typename T, typename Traits>
+struct supports_text_stream_insertion<Markable<T, Traits>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct supports_text_stream_insertion<OptionSet<T>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct supports_text_stream_insertion<std::optional<T>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct supports_text_stream_insertion<WeakPtr<T>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct supports_text_stream_insertion<RefPtr<T>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct supports_text_stream_insertion<Ref<T>> : supports_text_stream_insertion<T> { };
+
+template<typename T>
+struct ValueOrEllipsis {
+    explicit ValueOrEllipsis(const T& value)
+        : value(value)
+    { }
+    const T& value;
+};
+
+template<typename T>
+TextStream& operator<<(TextStream& ts, ValueOrEllipsis<T> item)
+{
+    if constexpr (supports_text_stream_insertion<T>::value)
+        ts << item.value;
+    else
+        ts << "...";
+    return ts;
+}
+
 // Deprecated. Use TextStream::writeIndent() instead.
 WTF_EXPORT_PRIVATE void writeIndent(TextStream&, int indent);
 
 } // namespace WTF
 
 using WTF::TextStream;
+using WTF::ValueOrEllipsis;
 using WTF::ValueOrNull;
 using WTF::indent;

Source/WebCore/platform/RuntimeApplicationChecks.cpp
@@ -83,4 +83,33 @@ bool checkAuxiliaryProcessType(AuxiliaryProcessType type)
     return *currentType == type; 
 }
 
+std::optional<AuxiliaryProcessType> processType()
+{
+    return auxiliaryProcessType();
+}
+
+const char* processTypeDescription(std::optional<AuxiliaryProcessType> type)
+{
+    if (!type)
+        return "UI";
+
+    switch (*type) {
+    case AuxiliaryProcessType::WebContent:
+        return "Web";
+    case AuxiliaryProcessType::Network:
+        return "Network";
+    case AuxiliaryProcessType::Plugin:
+        return "Plugin";
+#if ENABLE(GPU_PROCESS)
+    case AuxiliaryProcessType::GPU:
+        return "GPU";
+#endif
+#if ENABLE(WEB_AUTHN)
+    case AuxiliaryProcessType::WebAuthn:
+        return "WebAuthn";
+#endif
+    }
+    return "Unknown";
+}
+
 } // namespace WebCore

Source/WebCore/platform/RuntimeApplicationChecks.h
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include <optional>
 #include <wtf/Forward.h>
 
 namespace WebCore {
@@ -46,6 +47,8 @@ enum class AuxiliaryProcessType : uint8_t {
 
 WEBCORE_EXPORT void setAuxiliaryProcessType(AuxiliaryProcessType);
 WEBCORE_EXPORT bool checkAuxiliaryProcessType(AuxiliaryProcessType);
+WEBCORE_EXPORT std::optional<AuxiliaryProcessType> processType();
+WEBCORE_EXPORT const char* processTypeDescription(std::optional<AuxiliaryProcessType>);
 
 bool isInAuxiliaryProcess();
 inline bool isInWebProcess() { return checkAuxiliaryProcessType(AuxiliaryProcessType::WebContent); }

Source/WebKit/Platform/Logging.h
@@ -56,6 +56,7 @@ extern "C" {
     M(Fullscreen) \
     M(Gamepad) \
     M(IPC) \
+    M(IPCMessages) \
     M(ITPDebug) \
     M(IconDatabase) \
     M(Images) \

Source/WebKit/Scripts/webkit/messages.py
@@ -567,17 +567,17 @@ def async_message_statement(receiver, message):
     dispatch_function = 'handleMessage'
     if message.has_attribute(ASYNC_ATTRIBUTE):
         dispatch_function += 'Async'
-        dispatch_function_args.insert(0, 'connection')
 
     if message.has_attribute(WANTS_CONNECTION_ATTRIBUTE):
-        if message.has_attribute(ASYNC_ATTRIBUTE):
-            dispatch_function += 'WantsConnection'
-        else:
-            dispatch_function_args.insert(0, 'connection')
+        dispatch_function += 'WantsConnection'
+
+    connection = 'connection'
+    if receiver.has_attribute(STREAM_ATTRIBUTE):
+        connection = 'connection.connection()'
 
     result = []
     result.append('    if (decoder.messageName() == Messages::%s::%s::name())\n' % (receiver.name, message.name))
-    result.append('        return IPC::%s<Messages::%s::%s>(%s);\n' % (dispatch_function, receiver.name, message.name, ', '.join(dispatch_function_args)))
+    result.append('        return IPC::%s<Messages::%s::%s>(%s, %s);\n' % (dispatch_function, receiver.name, message.name, connection, ', '.join(dispatch_function_args)))
     return surround_in_condition(''.join(result), message.condition)
 
 
@@ -585,12 +585,11 @@ def sync_message_statement(receiver, message):
     dispatch_function = 'handleMessage'
     if message.has_attribute(SYNCHRONOUS_ATTRIBUTE):
         dispatch_function += 'Synchronous'
-        if message.has_attribute(WANTS_CONNECTION_ATTRIBUTE):
-            dispatch_function += 'WantsConnection'
     if message.has_attribute(ASYNC_ATTRIBUTE):
         dispatch_function += 'Async'
+    if message.has_attribute(WANTS_CONNECTION_ATTRIBUTE):
+        dispatch_function += 'WantsConnection'
 
-    wants_connection = message.has_attribute(SYNCHRONOUS_ATTRIBUTE) or message.has_attribute(WANTS_CONNECTION_ATTRIBUTE)
     maybe_reply_encoder = ", *replyEncoder"
     if receiver.has_attribute(STREAM_ATTRIBUTE):
         maybe_reply_encoder = ''
@@ -599,7 +598,7 @@ def sync_message_statement(receiver, message):
 
     result = []
     result.append('    if (decoder.messageName() == Messages::%s::%s::name())\n' % (receiver.name, message.name))
-    result.append('        return IPC::%s<Messages::%s::%s>(%sdecoder%s, this, &%s);\n' % (dispatch_function, receiver.name, message.name, 'connection, ' if wants_connection else '', maybe_reply_encoder, handler_function(receiver, message)))
+    result.append('        return IPC::%s<Messages::%s::%s>(connection, decoder%s, this, &%s);\n' % (dispatch_function, receiver.name, message.name, maybe_reply_encoder, handler_function(receiver, message)))
     return surround_in_condition(''.join(result), message.condition)
 
 

Source/WebKit/Scripts/webkit/tests/TestWithCVPixelBufferMessageReceiver.cpp
@@ -42,7 +42,7 @@ void TestWithCVPixelBuffer::didReceiveMessage(IPC::Connection& connection, IPC::
     auto protectedThis = makeRef(*this);
 #if USE(AVFOUNDATION)
     if (decoder.messageName() == Messages::TestWithCVPixelBuffer::SendCVPixelBuffer::name())
-        return IPC::handleMessage<Messages::TestWithCVPixelBuffer::SendCVPixelBuffer>(decoder, this, &TestWithCVPixelBuffer::sendCVPixelBuffer);
+        return IPC::handleMessage<Messages::TestWithCVPixelBuffer::SendCVPixelBuffer>(connection, decoder, this, &TestWithCVPixelBuffer::sendCVPixelBuffer);
 #endif
     UNUSED_PARAM(connection);
     UNUSED_PARAM(decoder);
@@ -58,7 +58,7 @@ bool TestWithCVPixelBuffer::didReceiveSyncMessage(IPC::Connection& connection, I
     auto protectedThis = makeRef(*this);
 #if USE(AVFOUNDATION)
     if (decoder.messageName() == Messages::TestWithCVPixelBuffer::ReceiveCVPixelBuffer::name())
-        return IPC::handleMessage<Messages::TestWithCVPixelBuffer::ReceiveCVPixelBuffer>(decoder, *replyEncoder, this, &TestWithCVPixelBuffer::receiveCVPixelBuffer);
+        return IPC::handleMessage<Messages::TestWithCVPixelBuffer::ReceiveCVPixelBuffer>(connection, decoder, *replyEncoder, this, &TestWithCVPixelBuffer::receiveCVPixelBuffer);
 #endif
     UNUSED_PARAM(connection);
     UNUSED_PARAM(decoder);

Source/WebKit/Scripts/webkit/tests/TestWithIfMessageMessageReceiver.cpp
@@ -42,11 +42,11 @@ void TestWithIfMessage::didReceiveMessage(IPC::Connection& connection, IPC::Deco
     auto protectedThis = makeRef(*this);
 #if PLATFORM(COCOA)
     if (decoder.messageName() == Messages::TestWithIfMessage::LoadURL::name())
-        return IPC::handleMessage<Messages::TestWithIfMessage::LoadURL>(decoder, this, &TestWithIfMessage::loadURL);
+        return IPC::handleMessage<Messages::TestWithIfMessage::LoadURL>(connection, decoder, this, &TestWithIfMessage::loadURL);
 #endif
 #if PLATFORM(GTK)
     if (decoder.messageName() == Messages::TestWithIfMessage::LoadURL::name())
-        return IPC::handleMessage<Messages::TestWithIfMessage::LoadURL>(decoder, this, &TestWithIfMessage::loadURL);
+        return IPC::handleMessage<Messages::TestWithIfMessage::LoadURL>(connection, decoder, this, &TestWithIfMessage::loadURL);
 #endif
     UNUSED_PARAM(connection);
     UNUSED_PARAM(decoder);

commit 13a8e05770d20245956c73c6180968bc59c0c712

REGRESSION(r282115): undefined reference to `IPC::messageArgumentDescriptions(IPC::MessageName)' in Debug build https://bugs.webkit.org/show_bug.cgi?id=230064
Unreviewed build fix.

Debug build of non-Cocoa ports were broken since r282115 started
to use IPC::messageArgumentDescriptions for debug logging.
However, MessageArgumentDescriptions.cpp has a long-standing
compilation problem for non-Cocoa ports.

Temporarily disabled the debug logging for non-Cocoa ports by
using ENABLE(IPC_TESTING_API) macro. I'll revert this change after
I will fix the fundamental problem of messages.py.

* Platform/IPC/HandleMessage.h:
(IPC::logMessageImpl):
(IPC::logReply):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@282177 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Platform/IPC/HandleMessage.h
@@ -47,7 +47,7 @@ class Connection;
 
 constexpr unsigned loggingContainerSizeLimit = 200;
 
-#if !LOG_DISABLED
+#if !LOG_DISABLED && ENABLE(IPC_TESTING_API)
 enum class ForReply : bool { No, Yes };
 
 inline TextStream textStreamForLogging(const Connection& connection, MessageName messageName, ForReply forReply)
@@ -73,7 +73,7 @@ inline TextStream textStreamForLogging(const Connection& connection, MessageName
 template<typename ArgsTuple, size_t... ArgsIndex>
 void logMessageImpl(const Connection& connection, MessageName messageName, const ArgsTuple& args, std::index_sequence<ArgsIndex...>)
 {
-#if !LOG_DISABLED
+#if !LOG_DISABLED && ENABLE(IPC_TESTING_API)
     auto stream = textStreamForLogging(connection, messageName, ForReply::No);
 
     if (auto argumentDescriptions = messageArgumentDescriptions(messageName))
@@ -96,7 +96,7 @@ void logMessage(const Connection& connection, MessageName messageName, const Arg
 template<typename... T>
 void logReply(const Connection& connection, MessageName messageName, const T&... args)
 {
-#if !LOG_DISABLED
+#if !LOG_DISABLED && ENABLE(IPC_TESTING_API)
     if (!sizeof...(T))
         return;
 