yurikoles/qtwebkit

commit 3a7ff242121a09f7da9881368f6c692a1a79f600

[Modern Media Controls] put the localized information for tracks first if the track is not in the user's preferred language https://bugs.webkit.org/show_bug.cgi?id=226361 <rdar://problem/78497853>
Reviewed by Eric Carlson.

Source/WebCore:

Test: media/modern-media-controls/tracks-support/sorted-by-user-preferred-languages.html

This makes it clearer/easier for the user to understand what's going on when the track label
is not in the user's preferred language. For example, if a `<video>` has an "English" audio
track, it's not very helpful to show "English (英语)" for Chinese users instead of something
like "英语 (English)".

If the track has a `label` and either matches the user's preferred language or its `label`
already contains the localized language name, combine the track's `label` with a localized
description of the track if that description isn't already in the `label`. Otherwise, add
the localized language name first, then the description of the track, and finally the `label`.

Here are some examples if the user's preferred language is English:
 - `<track kind="subtitles" srclang="en">`                  would show "English"
 - `<track kind="subtitles" srclang="en" label="Foo">`      would show "Foo"
 - `<track kind="captions"  srclang="en">`                  would show "English Captions"
 - `<track kind="captions"  srclang="en" label="Foo">`      would show "Foo Captions"
 - `<track kind="captions"  srclang="en" label="Captions">` would show "Captions"
 - `<track kind="subtitles" srclang="fr">`                  would show "French"
 - `<track kind="subtitles" srclang="fr" label="Foo">`      would show "French (Foo)"
 - `<track kind="captions"  srclang="fr">`                  would show "French Captions"
 - `<track kind="captions"  srclang="fr" label="Foo">`      would show "French Captions (Foo)"
 - `<track kind="captions"  srclang="fr" label="Captions">` would show "French Captions"

* page/CaptionUserPreferences.cpp:
(WebCore::trackDisplayName):

Source/WebCore/en.lproj/Localizable.strings
@@ -16,44 +16,44 @@
 /* visible name of the plug-in host process. The first argument is the plug-in name and the second argument is the application name. */
 "%@ (%@ Internet plug-in)" = "%@ (%@ Internet plug-in)";
 
-/* Text track display name format that includes the language of the subtitle, in the form of 'Title (Language)' */
-"%@ (%@)" = "%@ (%@)";
+/* Audio/Text track display name format that includes the label and language of the track, in the form of 'Language (Label)'. */
+"%@ (%@) (audio/text track)" = "%@ (%@)";
 
-/* Text track display name format that includes the country and language of the subtitle, in the form of 'Title (Language-Country)' */
-"%@ (%@-%@)" = "%@ (%@-%@)";
-
-/* Text track contains Audio Descriptions */
-"%@ AD" = "%@ AD";
-
-/* Text track contains closed captions */
-"%@ CC" = "%@ CC";
+/* Closed captions text track display name format that includes the language and/or locale (e.g. 'English CC'). */
+"%@ CC (text track)" = "%@ CC";
 
 /* Captions text track display name format that includes the language and/or locale (e.g. 'English Captions'). */
-"%@ Captions" = "%@ Captions";
+"%@ Captions (text track)" = "%@ Captions";
 
 /* Chapters text track display name format that includes the language and/or locale (e.g. 'English Chapters'). */
-"%@ Chapters" = "%@ Chapters";
+"%@ Chapters (text track)" = "%@ Chapters";
+
+/* Commentary audio track display name format that includes the language and/or locale (e.g. 'English Commentary'). */
+"%@ Commentary (audio track)" = "%@ Commentary";
+
+/* Descriptions audio track display name format that includes the language and/or locale (e.g. 'English Descriptions'). */
+"%@ Descriptions (audio track)" = "%@ Descriptions";
 
 /* Descriptions text track display name format that includes the language and/or locale (e.g. 'English Descriptions'). */
-"%@ Descriptions" = "%@ Descriptions";
+"%@ Descriptions (text track)" = "%@ Descriptions";
 
-/* Text track contains simplified (3rd grade level) subtitles */
-"%@ Easy Reader" = "%@ Easy Reader";
+/* Easy Reader (i.e. 3rd-grade level) text track display name format that includes the language and/or locale (e.g. 'English Easy Reader'). */
+"%@ Easy Reader (text track)" = "%@ Easy Reader";
 
-/* Text track contains forced subtitles */
-"%@ Forced" = "%@ Forced";
+/* Forced text track display name format that includes the language and/or locale (e.g. 'English Forced'). */
+"%@ Forced (text track)" = "%@ Forced";
 
 /* visible name of the GPU process. The argument is the application name. */
 "%@ Graphics and Media" = "%@ Graphics and Media";
 
-/* Metadata text track display name format that includes the language and/or locale (e.g. 'English Metadats'). */
-"%@ Metadata" = "%@ Metadata";
+/* Metadata text track display name format that includes the language and/or locale (e.g. 'English Metadata'). */
+"%@ Metadata (text track)" = "%@ Metadata";
 
 /* visible name of the network process. The argument is the application name. */
 "%@ Networking" = "%@ Networking";
 
-/* Text track contains subtitles for the deaf and hard of hearing */
-"%@ SDH" = "%@ SDH";
+/* SDH (i.e. deaf and/or hard of hearing) text track display name format that includes the language and/or locale (e.g. 'English SDH').  */
+"%@ SDH (text track)" = "%@ SDH";
 
 /* Visible name of Service Worker process. The argument is the application name. */
 "%@ Service Worker (%@)" = "%@ Service Worker (%@)";
@@ -181,8 +181,8 @@
 /* WKErrorAttributedStringContentFailedToLoad description */
 "Attributed string content failed to load" = "Attributed string content failed to load";
 
-/* Menu item label for automatic track selection behavior */
-"Auto (Recommended)" = "Auto (Recommended)";
+/* Menu item label for automatic track selection behavior. */
+"Auto (Recommended) (text track)" = "Auto (Recommended)";
 
 /* Back context menu item */
 "Back" = "Back";
@@ -208,8 +208,8 @@
 /* Label for the buy with Apple Pay button. */
 "Buy with Apple Pay" = "Buy with Apple Pay";
 
-/* Display name for closed captions text tracks that is used to see if the HTML 'label' already contains 'CC'. */
-"CC" = "CC";
+/* Display name for closed captions text tracks. */
+"CC (text track)" = "CC";
 
 /* Cancel */
 "Cancel" = "Cancel";
@@ -223,8 +223,8 @@
 /* Capitalize context menu item */
 "Capitalize" = "Capitalize";
 
-/* Display name for text track kind 'captions' that is used to see if the HTML 'label' already contains 'Captions'. */
-"Captions" = "Captions";
+/* Display name for text track kind 'captions'. */
+"Captions (text track)" = "Captions";
 
 /* Undo action name */
 "Center (Undo action name)" = "Center";
@@ -232,8 +232,8 @@
 /* Undo action name */
 "Change Attributes (Undo action name)" = "Change Attributes";
 
-/* Display name for text track kind 'chapters' that is used to see if the HTML 'label' already contains 'Chapters'. */
-"Chapters" = "Chapters";
+/* Display name for text track kind 'chapters'. */
+"Chapters (text track)" = "Chapters";
 
 /* Check spelling context menu item */
 "Check Document Now" = "Check Document Now";
@@ -267,6 +267,9 @@
 
 /* Subtitle of the label to show on a snapshotted plug-in */
 "Click to restart" = "Click to restart";
+
+/* Display name for audio track kind 'commentary'. */
+"Commentary (audio track)" = "%@ Commentary";
 
 /* WKErrorContentRuleListStoreCompileFailed description */
 "Compiling a WKContentRuleList failed" = "Compiling a WKContentRuleList failed";
@@ -334,8 +337,11 @@
 /* Undo action name */
 "Delete (Undo action name)" = "Delete";
 
-/* Display name for text track kind 'descriptions' that is used to see if the HTML 'label' already contains 'Descriptions'. */
-"Descriptions" = "Descriptions";
+/* Display name for audio track kind 'descriptions'. */
+"Descriptions (audio track)" = "Descriptions";
+
+/* Display name for text track kind 'descriptions'. */
+"Descriptions (text track)" = "Descriptions";
 
 /* text to display in <details> tag when it has no <summary> child */
 "Details" = "Details";
@@ -382,8 +388,8 @@
 /* Undo action name */
 "Drag (Undo action name)" = "Drag";
 
-/* Display name for easy reader text tracks that is used to see if the HTML 'label' already contains 'Easy Reader'. */
-"Easy Reader" = "Easy Reader";
+/* Display name for easy reader (i.e. 3rd-grade level) text tracks. */
+"Easy Reader (text track)" = "Easy Reader";
 
 /* Video Enter Full Screen context menu item */
 "Enter Full Screen" = "Enter Full Screen";
@@ -430,8 +436,8 @@
 /* Font context sub-menu item */
 "Font" = "Font";
 
-/* Display name for text track kind 'forced' that is used to see if the HTML 'label' already contains 'Forced'. */
-"Forced" = "Forced";
+/* Display name for text track kind 'forced'. */
+"Forced (text track)" = "Forced";
 
 /* Undo action name */
 "Format Block (Undo action name)" = "Formatting";
@@ -592,8 +598,8 @@
 /* Malware confirmation dialog */
 "Merely visiting a site is sufficient for malware to install itself and harm your computer." = "Merely visiting a site is sufficient for malware to install itself and harm your computer.";
 
-/* Display name for text track kind 'metadata' that is used to see if the HTML 'label' already contains 'Metadata'. */
-"Metadata" = "Metadata";
+/* Display name for text track kind 'metadata'. */
+"Metadata (text track)" = "Metadata";
 
 /* Label text to be used when a plugin is missing */
 "Missing Plug-in" = "Missing Plug-in";
@@ -625,8 +631,8 @@
 /* OK button label in document password failure alert */
 "OK (document password failure alert)" = "OK";
 
-/* Menu item label for the track that represents disabling closed captions */
-"Off" = "Off";
+/* Menu item label for the track that represents disabling closed captions. */
+"Off (text track)" = "Off";
 
 /* Title for Open Link action button */
 "Open" = "Open";
@@ -745,8 +751,8 @@
 /* Right to Left context menu item */
 "Right to Left" = "Right to Left";
 
-/* Display name for deaf and hard of hearing subtitle text tracks that is used to see if the HTML 'label' already contains 'SDH'. */
-"SDH" = "SDH";
+/* Display name for SDH (i.e. deaf and/or hard of hearing) text tracks. */
+"SDH (text track)" = "SDH";
 
 /* Title of the Search button for zoomed form controls. */
 "Search" = "Search";
@@ -1033,14 +1039,11 @@
 /* Undo action name */
 "Underline (Undo action name)" = "Underline";
 
-/* Menu item label for an audio track that has no other name */
-"Unknown (audio track)" = "Unknown";
-
 /* Unknown filesize FTP directory listing item */
 "Unknown (filesize)" = "Unknown";
 
-/* Menu item label for a text track that has no other name */
-"Unknown (text track)" = "Unknown";
+/* Menu item label for a audio/text track that has no other name. */
+"Unknown (audio/text track)" = "Unknown";
 
 /* Undo action name */
 "Unlink (Undo action name)" = "Unlink";

commit 2546115ecfc5aa629e004b47811de42813e9bfe7

Fix consistency issues with localized strings and regenerate localizable.strings <https://webkit.org/b/232642>
Reviewed by Megan Gardner.

* en.lproj/Localizable.strings:
- Regenerate the file using update-webkit-localizable-strings.
- List of strings realphabetized, except as noted below.
("%@ (%@ Internet plug-in)"): Delete.
- Removed in r285047 for Bug 232462.

* platform/LocalizedStrings.cpp:
(WebCore::addTextTrackKindEasyReaderSuffix):
- Fix unlocalized string that regressed in r278202 for
  Bug 226361.
(WebCore::useBlockedPlugInContextMenuTitle):
- Remove unneeded call to WebCore::formatLocalizedString() since
  there are no placeholders to replace.

* platform/cocoa/LocalizedStringsCocoa.mm:
(WebCore::contextMenuItemTagSearchWeb):
(WebCore::contextMenuItemTagShowFonts):
- Switch from calling WebCore::localizedNSString() directly to
  using WEB_UI_NSSTRING() so these strings will be found by
  update-webkit-localizable-strings.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@285234 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/en.lproj/Localizable.strings
@@ -13,9 +13,6 @@
 /* window title for a standalone image (uses multiplication symbol, not x) */
 "%@ %@×%@ pixels" = "%@ %@×%@ pixels";
 
-/* visible name of the plug-in host process. The first argument is the plug-in name and the second argument is the application name. */
-"%@ (%@ Internet plug-in)" = "%@ (%@ Internet plug-in)";
-
 /* Audio/Text track display name format that includes the label and language of the track, in the form of 'Language (Label)'. */
 "%@ (%@) (audio/text track)" = "%@ (%@)";
 
@@ -103,8 +100,8 @@
 /* Present the element <select multiple> when a single <option> is selected (iOS only) */
 "1 Item" = "1 Item";
 
-/* 1× media controls context menu playback speed label */
-"1× (Media Controls Menu Playback Speed)" = "1×";
+/* Option in segmented control for choosing list type in text editing */
+"1. 2. 3." = "1. 2. 3.";
 
 /* 1.25× media controls context menu playback speed label */
 "1.25× (Media Controls Menu Playback Speed)" = "1.25×";
@@ -112,14 +109,14 @@
 /* 1.5× media controls context menu playback speed label */
 "1.5× (Media Controls Menu Playback Speed)" = "1.5×";
 
-/* Option in segmented control for choosing list type in text editing */
-"1. 2. 3." = "1. 2. 3.";
-
-/* 2× media controls context menu playback speed label */
-"2× (Media Controls Menu Playback Speed)" = "2×";
+/* 1× media controls context menu playback speed label */
+"1× (Media Controls Menu Playback Speed)" = "1×";
 
 /* Menu item title for KEYGEN pop-up menu */
 "2048 (High Grade)" = "2048 (High Grade)";
+
+/* 2× media controls context menu playback speed label */
+"2× (Media Controls Menu Playback Speed)" = "2×";
 
 /* Name of application's single WebCrypto master key in Keychain */
 "<application> WebCrypto Master Key" = "<application> WebCrypto Master Key";
@@ -130,14 +127,14 @@
 /* WKErrorJavaScriptExceptionOccurred description */
 "A JavaScript exception occurred" = "A JavaScript exception occurred";
 
-/* Add to Quick Note context menu item. */
-"Add to Quick Note" = "Add to Quick Note";
-
 /* Label for the add money with Apple Pay button. */
 "Add money with Apple Pay" = "Add money with Apple Pay";
 
 /* Title for Add to Photos action button */
 "Add to Photos" = "Add to Photos";
+
+/* Add to Quick Note context menu item. */
+"Add to Quick Note" = "Add to Quick Note";
 
 /* Title for Add to Reading List action button */
 "Add to Reading List" = "Add to Reading List";
@@ -157,23 +154,23 @@
 /* Allow button title in geolocation prompt */
 "Allow (geolocation)" = "Allow";
 
+/* Allow button title in screen sharing prompt */
+"Allow (screensharing)" = "Allow";
+
 /* Allow button title in speech recognition prompt */
 "Allow (speechrecognition)" = "Allow";
 
 /* Allow button title in user media prompt */
 "Allow (usermedia)" = "Allow";
 
-/* Allow button title in screen sharing prompt */
-"Allow (screensharing)" = "Allow";
-
 /* Message for requesting cross-site cookie and website data access. */
 "Allow “%@” and “%@” to use cookies and website data while browsing “%@”?" = "Allow “%@” and “%@” to use cookies and website data while browsing “%@”?";
 
-/* Message for geolocation prompt */
-"Allow “%@” to use your current location?" = "Allow “%@” to use your current location?";
-
 /* Message for spechrecognition prompt */
 "Allow “%@” to capture your audio and use it for speech recognition?" = "Allow “%@” to capture your audio and use it for speech recognition?";
+
+/* Message for screen sharing prompt */
+"Allow “%@” to observe your screen?" = "Allow “%@” to observe your screen?";
 
 /* Message for requesting cross-site cookie and website data access. */
 "Allow “%@” to use cookies and website data while browsing “%@”?" = "Allow “%@” to use cookies and website data while browsing “%@”?";
@@ -184,11 +181,11 @@
 /* Message for user camera access prompt */
 "Allow “%@” to use your camera?" = "Allow “%@” to use your camera?";
 
+/* Message for geolocation prompt */
+"Allow “%@” to use your current location?" = "Allow “%@” to use your current location?";
+
 /* Message for user microphone access prompt */
 "Allow “%@” to use your microphone?" = "Allow “%@” to use your microphone?";
-
-/* Message for screen sharing prompt. */
-"Allow “%@” to observe your screen?" = "Allow “%@” to observe your screen?";
 
 /* WKErrorUnknown description */
 "An unknown error occurred" = "An unknown error occurred";
@@ -391,14 +388,14 @@
 /* Disallow button title in geolocation prompt */
 "Don’t Allow (geolocation)" = "Don’t Allow";
 
+/* Disallow button title in screen sharing prompt */
+"Don’t Allow (screensharing)" = "Don’t Allow";
+
 /* Disallow button title in speech recognition prompt */
 "Don’t Allow (speechrecognition)" = "Don’t Allow";
 
 /* Disallow button title in user media prompt */
 "Don’t Allow (usermedia)" = "Don’t Allow";
-
-/* Disallow button title in screen sharing prompt */
-"Don’t Allow (screensharing)" = "Don’t Allow";
 
 /* Download Audio To Disk context menu item */
 "Download Audio" = "Download Audio";
@@ -739,11 +736,11 @@
 /* Media Play context menu item */
 "Play" = "Play";
 
-/* Playback Speed media controls context menu title */
-"Playback Speed (Media Controls Menu)" = "Playback Speed";
-
 /* accessibility help text for enter full screen button */
 "Play movie in full screen mode" = "Play movie in full screen mode";
+
+/* Playback Speed media controls context menu title */
+"Playback Speed (Media Controls Menu)" = "Playback Speed";
 
 /* Label text to be used when a plug-in was blocked from loading because it was too small */
 "Plug-In too small" = "Plug-In too small";
@@ -1090,11 +1087,11 @@
 /* Undo action name */
 "Underline (Undo action name)" = "Underline";
 
-/* Unknown filesize FTP directory listing item */
-"Unknown (filesize)" = "Unknown";
-
 /* Menu item label for a audio/text track that has no other name. */
 "Unknown (audio/text track)" = "Unknown";
+
+/* Unknown filesize FTP directory listing item */
+"Unknown (filesize)" = "Unknown";
 
 /* Undo action name */
 "Unlink (Undo action name)" = "Unlink";
@@ -1639,6 +1636,9 @@
 /* Message for requesting access to the device motion and orientation */
 "“%@” Would Like to Access Motion and Orientation" = "“%@” Would Like to Access Motion and Orientation";
 
+/* The domain and application using the camera and/or microphone. The first argument is domain, the second is the application name (iOS only). */
+"“%@” in “%%@”" = "“%@” in “%%@”";
+
 /* Allow the specified bundle to sign in to the specified website */
 "“%@” would like to sign in to “%@”." = "“%@” would like to sign in to “%@”.";
 
@@ -1648,5 +1648,3 @@
 /* Option in segmented control for choosing list type in text editing */
 "•" = "•";
 
-/* The domain and application using the camera and/or microphone. The first argument is domain, the second is the application name (iOS only). */
-"“%@” in “%%@”" = "“%@” in “%%@”";

Source/WebCore/platform/LocalizedStrings.cpp
@@ -1193,7 +1193,7 @@ String addTextTrackKindEasyReaderSuffix(const String& text)
 {
     if (text.isEmpty())
         return textTrackKindEasyReaderDisplayName();
-    return formatLocalizedString(WEB_UI_CFSTRING_KEY("%@ Easy Reader", "%@ Easy (text track)", "Easy Reader (i.e. 3rd-grade level) text track display name format that includes the language and/or locale (e.g. 'English Easy Reader')."), text.createCFString().get());
+    return formatLocalizedString(WEB_UI_CFSTRING_KEY("%@ Easy Reader", "%@ Easy Reader (text track)", "Easy Reader (i.e. 3rd-grade level) text track display name format that includes the language and/or locale (e.g. 'English Easy Reader')."), text.createCFString().get());
 }
 
 String textTrackKindForcedDisplayName()
@@ -1248,7 +1248,7 @@ String snapshottedPlugInLabelSubtitle()
 
 String useBlockedPlugInContextMenuTitle()
 {
-    return formatLocalizedString(WEB_UI_STRING("Show in blocked plug-in", "Title of the context menu item to show when PDFPlugin was used instead of a blocked plugin"));
+    return WEB_UI_STRING("Show in blocked plug-in", "Title of the context menu item to show when PDFPlugin was used instead of a blocked plugin");
 }
 
 #if ENABLE(WEB_CRYPTO)

Source/WebCore/platform/cocoa/LocalizedStringsCocoa.mm
@@ -57,15 +57,16 @@ String copyImageUnknownFileLabel()
 {
     return WEB_UI_STRING("unknown", "Unknown filename");
 }
+
 #if ENABLE(APP_HIGHLIGHTS)
 String contextMenuItemTagAddHighlightToCurrentQuickNote()
 {
-    return localizedNSString(@"Add to Quick Note");
+    return WEB_UI_NSSTRING(@"Add to Quick Note", "Add to Quick Note context menu item.");
 }
 
 String contextMenuItemTagAddHighlightToNewQuickNote()
 {
-    return localizedNSString(@"New Quick Note");
+    return WEB_UI_NSSTRING(@"New Quick Note", "New Quick Note context menu item.");
 }
 #endif
 