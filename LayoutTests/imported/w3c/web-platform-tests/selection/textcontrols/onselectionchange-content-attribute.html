night-codes/WebKit

commit 7536e4c6c5bcab11e0c10d3d50355e1f66331602

Introduce Selection specific mixin of GlobalEventHandlers https://bugs.webkit.org/show_bug.cgi?id=217248
Patch by Rob Buis <rbuis@igalia.com> on 2020-10-20
Reviewed by Sam Weinig.

Source/WebCore:

Introduce Selection specific mixin of GlobalEventHandlers [1] by
splitting out the interface from Document+Selection.idl. This
also means the HTMLBodyElement specific selectionchange IDL is
superflous and can be removed.

Behavior matches Chrome and Firefox.

Test: fast/dom/event-handler-attributes.html

[1] https://w3c.github.io/selection-api/#dom-globaleventhandlers

* CMakeLists.txt:
* DerivedSources-input.xcfilelist:
* DerivedSources-output.xcfilelist:
* DerivedSources.make:
* WebCore.xcodeproj/project.pbxproj:
* dom/Document+Selection.idl:
* dom/Element.idl:
* dom/GlobalEventHandlers+Selection.idl: Copied from Source/WebCore/dom/Document+Selection.idl.

LayoutTests:

Adapt test to GlobalEventHandlers change.

* fast/dom/event-attribute-availability-expected.txt:
* fast/dom/event-handler-attributes-expected.txt:
* fast/dom/event-handler-attributes.html:

Identifier: 230693@main
git-svn-id: https://svn.webkit.org/repository/webkit/trunk@268745 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/dom/event-handler-attributes-expected.txt
@@ -65,6 +65,8 @@ PASS testScriptAttribute(window, "search") is "window"
 PASS testScriptAttribute(window, "seeked") is "window"
 PASS testScriptAttribute(window, "seeking") is "window"
 PASS testScriptAttribute(window, "select") is "window"
+PASS testScriptAttribute(window, "selectionchange") is "window"
+PASS testScriptAttribute(window, "selectstart") is "window"
 PASS testScriptAttribute(window, "stalled") is "window"
 PASS testScriptAttribute(window, "storage") is "window"
 PASS testScriptAttribute(window, "submit") is "window"
@@ -93,8 +95,6 @@ PASS testScriptAttribute(window, "focusout") is "none"
 PASS testScriptAttribute(window, "noneventname") is "none"
 PASS testScriptAttribute(window, "paste") is "none"
 PASS testScriptAttribute(window, "readystatechange") is "none"
-PASS testScriptAttribute(window, "selectionchange") is "none"
-PASS testScriptAttribute(window, "selectstart") is "none"
 
 Event names we expect to be able to set on the document
 
@@ -252,6 +252,7 @@ PASS testElementAttribute(element, "search") is "target"
 PASS testElementAttribute(element, "seeked") is "target"
 PASS testElementAttribute(element, "seeking") is "target"
 PASS testElementAttribute(element, "select") is "target"
+FAIL testElementAttribute(element, "selectionchange") should be target. Was script: target; content: none.
 PASS testElementAttribute(element, "selectstart") is "target"
 PASS testElementAttribute(element, "stalled") is "target"
 PASS testElementAttribute(element, "submit") is "target"
@@ -278,7 +279,6 @@ PASS testElementAttribute(element, "pagehide") is "none"
 PASS testElementAttribute(element, "pageshow") is "none"
 PASS testElementAttribute(element, "popstate") is "none"
 PASS testElementAttribute(element, "readystatechange") is "none"
-PASS testElementAttribute(element, "selectionchange") is "none"
 PASS testElementAttribute(element, "storage") is "none"
 PASS testElementAttribute(element, "unload") is "none"
 
@@ -345,6 +345,7 @@ PASS testElementAttribute(inputElement, "search") is "target"
 PASS testElementAttribute(inputElement, "seeked") is "target"
 PASS testElementAttribute(inputElement, "seeking") is "target"
 PASS testElementAttribute(inputElement, "select") is "target"
+FAIL testElementAttribute(inputElement, "selectionchange") should be target. Was script: target; content: none.
 PASS testElementAttribute(inputElement, "selectstart") is "target"
 PASS testElementAttribute(inputElement, "stalled") is "target"
 PASS testElementAttribute(inputElement, "submit") is "target"
@@ -371,7 +372,6 @@ PASS testElementAttribute(inputElement, "pagehide") is "none"
 PASS testElementAttribute(inputElement, "pageshow") is "none"
 PASS testElementAttribute(inputElement, "popstate") is "none"
 PASS testElementAttribute(inputElement, "readystatechange") is "none"
-PASS testElementAttribute(inputElement, "selectionchange") is "none"
 PASS testElementAttribute(inputElement, "storage") is "none"
 PASS testElementAttribute(inputElement, "unload") is "none"
 
@@ -438,6 +438,7 @@ PASS testElementAttribute(audioElement, "search") is "target"
 PASS testElementAttribute(audioElement, "seeked") is "target"
 PASS testElementAttribute(audioElement, "seeking") is "target"
 PASS testElementAttribute(audioElement, "select") is "target"
+FAIL testElementAttribute(audioElement, "selectionchange") should be target. Was script: target; content: none.
 PASS testElementAttribute(audioElement, "selectstart") is "target"
 PASS testElementAttribute(audioElement, "stalled") is "target"
 PASS testElementAttribute(audioElement, "submit") is "target"
@@ -464,7 +465,6 @@ PASS testElementAttribute(audioElement, "pagehide") is "none"
 PASS testElementAttribute(audioElement, "pageshow") is "none"
 PASS testElementAttribute(audioElement, "popstate") is "none"
 PASS testElementAttribute(audioElement, "readystatechange") is "none"
-PASS testElementAttribute(audioElement, "selectionchange") is "none"
 PASS testElementAttribute(audioElement, "storage") is "none"
 PASS testElementAttribute(audioElement, "unload") is "none"
 
@@ -531,6 +531,7 @@ PASS testElementAttribute(videoElement, "search") is "target"
 PASS testElementAttribute(videoElement, "seeked") is "target"
 PASS testElementAttribute(videoElement, "seeking") is "target"
 PASS testElementAttribute(videoElement, "select") is "target"
+FAIL testElementAttribute(videoElement, "selectionchange") should be target. Was script: target; content: none.
 PASS testElementAttribute(videoElement, "selectstart") is "target"
 PASS testElementAttribute(videoElement, "stalled") is "target"
 PASS testElementAttribute(videoElement, "submit") is "target"
@@ -557,7 +558,6 @@ PASS testElementAttribute(videoElement, "pagehide") is "none"
 PASS testElementAttribute(videoElement, "pageshow") is "none"
 PASS testElementAttribute(videoElement, "popstate") is "none"
 PASS testElementAttribute(videoElement, "readystatechange") is "none"
-PASS testElementAttribute(videoElement, "selectionchange") is "none"
 PASS testElementAttribute(videoElement, "storage") is "none"
 PASS testElementAttribute(videoElement, "unload") is "none"
 
@@ -682,7 +682,7 @@ PASS testElementAttribute(framesetElement, "unload") is "window"
 
 Event names we expect to be forwarded from <frameset> element to document)
 
-FAIL testElementAttribute(framesetElement, "selectionchange") should be document. Was none.
+FAIL testElementAttribute(framesetElement, "selectionchange") should be document. Was script: target; content: none.
 
 Non-forwarded event names on <frameset> element
 
@@ -822,6 +822,7 @@ PASS testElementAttribute(rectElement, "search") is "target"
 PASS testElementAttribute(rectElement, "seeked") is "target"
 PASS testElementAttribute(rectElement, "seeking") is "target"
 PASS testElementAttribute(rectElement, "select") is "target"
+FAIL testElementAttribute(rectElement, "selectionchange") should be target. Was script: target; content: none.
 PASS testElementAttribute(rectElement, "selectstart") is "target"
 PASS testElementAttribute(rectElement, "stalled") is "target"
 PASS testElementAttribute(rectElement, "submit") is "target"
@@ -848,7 +849,6 @@ PASS testElementAttribute(rectElement, "pagehide") is "none"
 PASS testElementAttribute(rectElement, "pageshow") is "none"
 PASS testElementAttribute(rectElement, "popstate") is "none"
 PASS testElementAttribute(rectElement, "readystatechange") is "none"
-PASS testElementAttribute(rectElement, "selectionchange") is "none"
 PASS testElementAttribute(rectElement, "storage") is "none"
 PASS testElementAttribute(rectElement, "unload") is "none"
 
@@ -926,7 +926,7 @@ PASS testElementAttribute(nonHTMLElement, "seeked") is "none"
 PASS testElementAttribute(nonHTMLElement, "seeking") is "none"
 PASS testElementAttribute(nonHTMLElement, "select") is "none"
 PASS testElementAttribute(nonHTMLElement, "selectionchange") is "none"
-FAIL testElementAttribute(nonHTMLElement, "selectstart") should be none. Was script: target; content: none.
+PASS testElementAttribute(nonHTMLElement, "selectstart") is "none"
 PASS testElementAttribute(nonHTMLElement, "stalled") is "none"
 PASS testElementAttribute(nonHTMLElement, "storage") is "none"
 PASS testElementAttribute(nonHTMLElement, "submit") is "none"

LayoutTests/fast/dom/event-attribute-availability-expected.txt
@@ -160,7 +160,7 @@ FAIL 'onbeforepaste' in window should be true. Was false.
 FAIL 'onpaste' in window should be true. Was false.
 PASS 'onreset' in window is true
 PASS 'onsearch' in window is true
-FAIL 'onselectstart' in window should be true. Was false.
+PASS 'onselectstart' in window is true
 
 Test Text Node
 PASS 'onabort' in textNode is false

LayoutTests/fast/dom/event-handler-attributes.html
@@ -64,6 +64,8 @@ const eventsInGlobalEventHandlers = [
     "seeked",
     "seeking",
     "select",
+    "selectionchange",
+    "selectstart",
     "stalled",
     "submit",
     "suspend",
@@ -98,8 +100,7 @@ const elementAndDocumentEvents = [
     "beforepaste",
     "copy",
     "cut",
-    "paste",
-    "selectstart",
+    "paste"
 ];
 
 const elementAndWindowEvents = [
@@ -110,7 +111,6 @@ const elementAndWindowEvents = [
 ];
 
 const uniqueDocumentEvents = [
-    "selectionchange",
     "readystatechange"
 ];
 

Source/WebCore/CMakeLists.txt
@@ -828,6 +828,7 @@ set(WebCore_NON_SVG_IDL_FILES
     dom/EventTarget.idl
     dom/FocusEvent.idl
     dom/GlobalEventHandlers+PointerEvents.idl
+    dom/GlobalEventHandlers+Selection.idl
     dom/GlobalEventHandlers.idl
     dom/HashChangeEvent.idl
     dom/IdleDeadline.idl

Source/WebCore/DerivedSources-input.xcfilelist
@@ -752,6 +752,7 @@ $(PROJECT_DIR)/dom/EventTarget.idl
 $(PROJECT_DIR)/dom/EventTargetFactory.in
 $(PROJECT_DIR)/dom/FocusEvent.idl
 $(PROJECT_DIR)/dom/GlobalEventHandlers+PointerEvents.idl
+$(PROJECT_DIR)/dom/GlobalEventHandlers+Selection.idl
 $(PROJECT_DIR)/dom/GlobalEventHandlers.idl
 $(PROJECT_DIR)/dom/HashChangeEvent.idl
 $(PROJECT_DIR)/dom/IdleDeadline.idl

Source/WebCore/DerivedSources-output.xcfilelist
@@ -920,6 +920,8 @@ $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+CSSTransition
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+CSSTransitions.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+PointerEvents.cpp
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+PointerEvents.h
+$(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+Selection.cpp
+$(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers+Selection.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers.cpp
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSGlobalEventHandlers.h
 $(BUILT_PRODUCTS_DIR)/DerivedSources/WebCore/JSHTMLAllCollection.cpp

Source/WebCore/DerivedSources.make
@@ -709,6 +709,7 @@ JS_BINDING_IDLS = \
     $(WebCore)/dom/EventTarget.idl \
     $(WebCore)/dom/FocusEvent.idl \
     $(WebCore)/dom/GlobalEventHandlers+PointerEvents.idl \
+    $(WebCore)/dom/GlobalEventHandlers+Selection.idl \
     $(WebCore)/dom/GlobalEventHandlers.idl \
     $(WebCore)/dom/HashChangeEvent.idl \
     $(WebCore)/dom/IdleDeadline.idl \

Source/WebCore/dom/Document+Selection.idl
@@ -31,8 +31,4 @@ partial interface Document {
     // https://w3c.github.io/webcomponents/spec/shadow/#extensions-to-the-documentorshadowroot-mixin.
     // We should get this resolved in the Selection API spec and move this to the correct IDL file.
     DOMSelection? getSelection();
-
-    // FIXME: Should be moved to GlobalEventHandlers (http://w3c.github.io/selection-api/#extensions-to-globaleventhandlers).
-    [NotEnumerable] attribute EventHandler onselectstart; // FIXME: Should be enumerable.
-    [NotEnumerable] attribute EventHandler onselectionchange; // FIXME: Should be enumerable.
 };

yurikoles/qtwebkit

commit 77921413b76c0be7be5e72171adda584928c4996

Setting "onselectionchange" content attribute should add an event listener https://bugs.webkit.org/show_bug.cgi?id=234167
Reviewed by Darin Adler.

LayoutTests/imported/w3c:

Import WPT tests from https://github.com/web-platform-tests/wpt/pull/32013.

* web-platform-tests/selection/textcontrols: Added.
* web-platform-tests/selection/textcontrols/onselectionchange-content-attribute-expected.txt: Added.
* web-platform-tests/selection/textcontrols/onselectionchange-content-attribute.html: Added.

Source/WebCore:

This patch implements full support for "onselectionchange" content attribute,
enabling it to add an event listener on any element, which fixes regression
introduced in r268745 and aligns "onselectionchange" with "onselectstart".

Aligns WebKit with Gecko and the spec [1].

[1] https://w3c.github.io/selection-api/#extensions-to-globaleventhandlers-interface

Tests: fast/dom/event-handler-attributes.html
       imported/w3c/web-platform-tests/selection/textcontrols/onselectionchange-content-attribute.html

* html/HTMLElement.cpp:
(WebCore::HTMLElement::createEventHandlerNameMap):

LayoutTests:

* fast/dom/event-handler-attributes-expected.txt:


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@286898 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLElement.cpp
@@ -327,6 +327,7 @@ HTMLElement::EventHandlerNameMap HTMLElement::createEventHandlerNameMap()
         &onseekedAttr.get(),
         &onseekingAttr.get(),
         &onselectAttr.get(),
+        &onselectionchangeAttr.get(),
         &onselectstartAttr.get(),
         &onslotchangeAttr.get(),
         &onstalledAttr.get(),

LayoutTests/fast/dom/event-handler-attributes-expected.txt
@@ -252,7 +252,7 @@ PASS testElementAttribute(element, "search") is "target"
 PASS testElementAttribute(element, "seeked") is "target"
 PASS testElementAttribute(element, "seeking") is "target"
 PASS testElementAttribute(element, "select") is "target"
-FAIL testElementAttribute(element, "selectionchange") should be target. Was script: target; content: none.
+PASS testElementAttribute(element, "selectionchange") is "target"
 PASS testElementAttribute(element, "selectstart") is "target"
 PASS testElementAttribute(element, "stalled") is "target"
 PASS testElementAttribute(element, "submit") is "target"
@@ -345,7 +345,7 @@ PASS testElementAttribute(inputElement, "search") is "target"
 PASS testElementAttribute(inputElement, "seeked") is "target"
 PASS testElementAttribute(inputElement, "seeking") is "target"
 PASS testElementAttribute(inputElement, "select") is "target"
-FAIL testElementAttribute(inputElement, "selectionchange") should be target. Was script: target; content: none.
+PASS testElementAttribute(inputElement, "selectionchange") is "target"
 PASS testElementAttribute(inputElement, "selectstart") is "target"
 PASS testElementAttribute(inputElement, "stalled") is "target"
 PASS testElementAttribute(inputElement, "submit") is "target"
@@ -438,7 +438,7 @@ PASS testElementAttribute(audioElement, "search") is "target"
 PASS testElementAttribute(audioElement, "seeked") is "target"
 PASS testElementAttribute(audioElement, "seeking") is "target"
 PASS testElementAttribute(audioElement, "select") is "target"
-FAIL testElementAttribute(audioElement, "selectionchange") should be target. Was script: target; content: none.
+PASS testElementAttribute(audioElement, "selectionchange") is "target"
 PASS testElementAttribute(audioElement, "selectstart") is "target"
 PASS testElementAttribute(audioElement, "stalled") is "target"
 PASS testElementAttribute(audioElement, "submit") is "target"
@@ -531,7 +531,7 @@ PASS testElementAttribute(videoElement, "search") is "target"
 PASS testElementAttribute(videoElement, "seeked") is "target"
 PASS testElementAttribute(videoElement, "seeking") is "target"
 PASS testElementAttribute(videoElement, "select") is "target"
-FAIL testElementAttribute(videoElement, "selectionchange") should be target. Was script: target; content: none.
+PASS testElementAttribute(videoElement, "selectionchange") is "target"
 PASS testElementAttribute(videoElement, "selectstart") is "target"
 PASS testElementAttribute(videoElement, "stalled") is "target"
 PASS testElementAttribute(videoElement, "submit") is "target"
@@ -682,7 +682,7 @@ PASS testElementAttribute(framesetElement, "unload") is "window"
 
 Event names we expect to be forwarded from <frameset> element to document)
 
-FAIL testElementAttribute(framesetElement, "selectionchange") should be document. Was script: target; content: none.
+FAIL testElementAttribute(framesetElement, "selectionchange") should be document. Was target.
 
 Non-forwarded event names on <frameset> element
 
@@ -822,7 +822,7 @@ PASS testElementAttribute(rectElement, "search") is "target"
 PASS testElementAttribute(rectElement, "seeked") is "target"
 PASS testElementAttribute(rectElement, "seeking") is "target"
 PASS testElementAttribute(rectElement, "select") is "target"
-FAIL testElementAttribute(rectElement, "selectionchange") should be target. Was script: target; content: none.
+PASS testElementAttribute(rectElement, "selectionchange") is "target"
 PASS testElementAttribute(rectElement, "selectstart") is "target"
 PASS testElementAttribute(rectElement, "stalled") is "target"
 PASS testElementAttribute(rectElement, "submit") is "target"

LayoutTests/imported/w3c/web-platform-tests/selection/textcontrols/onselectionchange-content-attribute-expected.txt
@@ -1,0 +1,4 @@
+
+PASS handler set from parser
+PASS handler set via setAttribute()
+

LayoutTests/imported/w3c/web-platform-tests/selection/textcontrols/onselectionchange-content-attribute.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Test that setting "onselectionchange" content attribute adds an event listener</title>
+<link rel="help" href="https://w3c.github.io/selection-api/#extensions-to-globaleventhandlers-interface">
+
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+
+<body>
+<div id="testElement" onselectionchange="window.handlerSetFromParserWasFired()"></div>
+<script>
+  promise_test(() => {
+    return new Promise(resolve => {
+      window.handlerSetFromParserWasFired = resolve;
+      testElement.dispatchEvent(new Event("selectionchange"));
+    });
+  }, "handler set from parser");
+
+  promise_test(() => {
+    const el = document.createElement("div");
+    el.setAttribute("onselectionchange", "window.handlerSetViaSetAttributeWasFired()");
+    document.body.append(el);
+
+    return new Promise(resolve => {
+      window.handlerSetViaSetAttributeWasFired = resolve;
+      el.dispatchEvent(new Event("selectionchange"));
+    });
+  }, "handler set via setAttribute()");
+</script>