yurikoles/qtwebkit

commit da062e1d0975e1f9d3b91d5efbec82cd3e7b1085

Clean up / optimize call sites constructing vectors https://bugs.webkit.org/show_bug.cgi?id=236748
Reviewed by Dean Jackson.

Clean up / optimize call sites constructing vectors:
- Use Vector::map() / WTF::map() where appropriate
Source/JavaScriptCore:

- Replace Vector::reserveCapacity() with Vector::reserveInitialCapacity()
  where appropriate
- Replace Vector::append() with Vector::uncheckedAppend() where appropriate
- Leverage the fact that Vector<Ref<Foo>> is now copyable (Because Ref<> now has
  a copy constructor)
- Call crossThreadCopy() to isolate-copy a Vector() without iterating.
- Leverage Vector constructor that takes in an intialization list in more cases
- Leverage Vector::from() in more cases to simplify vector construction in some
  case.
- Drop unnecessary calls to Vector::reserve*Capacity() before calling
  Vector::append(T*, size_t) since the append() implementation already does this
  for us.
- Leverage copyToVector() and copyToVectorOf<> in more cases
- Call shrinkToFit() in some cases where we reserve capacity and then append()
  but may not append() as much as we reserved.

* b3/air/AirCode.cpp:
(JSC::B3::Air::Code::setNumEntrypoints):
* bytecompiler/NodesCodegen.cpp:
(JSC::ArrayPatternNode::emitDirectBinding):
* dfg/DFGInPlaceAbstractState.cpp:
(JSC::DFG::setLiveValues):
* dfg/DFGLivenessAnalysisPhase.cpp:
* dfg/DFGScoreBoard.h:
(JSC::DFG::ScoreBoard::ScoreBoard):
* inspector/agents/InspectorDebuggerAgent.cpp:
(Inspector::parseBreakpointOptions):
* inspector/remote/socket/RemoteInspectorMessageParser.cpp:
(Inspector::MessageParser::pushReceivedData):
* parser/VariableEnvironment.cpp:
(JSC::CompactTDZEnvironment::CompactTDZEnvironment):
* wasm/WasmWorklist.cpp:
(JSC::Wasm::Worklist::Worklist):

Source/WebCore:

- Replace Vector::reserveCapacity() with Vector::reserveInitialCapacity()
  where appropriate
- Replace Vector::append() with Vector::uncheckedAppend() where appropriate

Source/WebKit/UIProcess/API/APIWebsitePolicies.cpp
@@ -59,18 +59,8 @@ Ref<WebsitePolicies> WebsitePolicies::copy() const
     policies->setWebsiteDataStore(m_websiteDataStore.get());
     policies->setUserContentController(m_userContentController.get());
     policies->setIdempotentModeAutosizingOnlyHonorsPercentages(m_idempotentModeAutosizingOnlyHonorsPercentages);
-    
-    Vector<WebCore::HTTPHeaderField> legacyCustomHeaderFields;
-    legacyCustomHeaderFields.reserveInitialCapacity(m_legacyCustomHeaderFields.size());
-    for (auto& field : m_legacyCustomHeaderFields)
-        legacyCustomHeaderFields.uncheckedAppend(field);
-    policies->setLegacyCustomHeaderFields(WTFMove(legacyCustomHeaderFields));
-
-    Vector<WebCore::CustomHeaderFields> customHeaderFields;
-    customHeaderFields.reserveInitialCapacity(m_customHeaderFields.size());
-    for (auto& field : m_customHeaderFields)
-        customHeaderFields.uncheckedAppend(field);
-    policies->setCustomHeaderFields(WTFMove(customHeaderFields));
+    policies->setLegacyCustomHeaderFields(Vector { m_legacyCustomHeaderFields });
+    policies->setCustomHeaderFields(Vector { m_customHeaderFields });
     policies->setAllowSiteSpecificQuirksToOverrideContentMode(m_allowSiteSpecificQuirksToOverrideContentMode);
     policies->setApplicationNameForDesktopUserAgent(m_applicationNameForDesktopUserAgent);
     policies->setAllowsContentJavaScript(m_allowsContentJavaScript);
@@ -98,8 +88,7 @@ WebKit::WebsitePoliciesData WebsitePolicies::data()
     bool hasLegacyCustomHeaderFields = legacyCustomHeaderFields().size();
     Vector<WebCore::CustomHeaderFields> customHeaderFields;
     customHeaderFields.reserveInitialCapacity(this->customHeaderFields().size() + hasLegacyCustomHeaderFields);
-    for (auto& field : this->customHeaderFields())
-        customHeaderFields.uncheckedAppend(field);
+    customHeaderFields.appendVector(this->customHeaderFields());
     if (hasLegacyCustomHeaderFields)
         customHeaderFields.uncheckedAppend({ legacyCustomHeaderFields(), { }});
 

commit 3be195fb9df59e9b35fbafeede695e76e285a5fd

[GTK] Debian Stable build is broken after r290026 https://bugs.webkit.org/show_bug.cgi?id=236819
Unreviewed build fix.


* UIProcess/API/APIWebsitePolicies.cpp:
(API::WebsitePolicies::copy const):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@290106 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/API/APIWebsitePolicies.cpp
@@ -59,8 +59,8 @@ Ref<WebsitePolicies> WebsitePolicies::copy() const
     policies->setWebsiteDataStore(m_websiteDataStore.get());
     policies->setUserContentController(m_userContentController.get());
     policies->setIdempotentModeAutosizingOnlyHonorsPercentages(m_idempotentModeAutosizingOnlyHonorsPercentages);
-    policies->setLegacyCustomHeaderFields(Vector { m_legacyCustomHeaderFields });
-    policies->setCustomHeaderFields(Vector { m_customHeaderFields });
+    policies->setLegacyCustomHeaderFields(Vector<WebCore::HTTPHeaderField> { m_legacyCustomHeaderFields });
+    policies->setCustomHeaderFields(Vector<WebCore::CustomHeaderFields> { m_customHeaderFields });
     policies->setAllowSiteSpecificQuirksToOverrideContentMode(m_allowSiteSpecificQuirksToOverrideContentMode);
     policies->setApplicationNameForDesktopUserAgent(m_applicationNameForDesktopUserAgent);
     policies->setAllowsContentJavaScript(m_allowsContentJavaScript);